c08c5158eed9970d9cd226c30df02619397dbe03
Sam Ruby <rubys@apache.org>
2000-04-27 13:53:14 +0000

    Added convenience for JDK 1.2+ users Submitted by: Conor MacNeill <conor@m64.com>
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@267659 13f79535-47bb-0310-9956-ffa450edef68

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 @echo off
 REM convience bat file to build with
+if exist %JAVA_HOME%\lib\tools.jar set CLASSPATH=%CLASSPATH%;%JAVA_HOME%\lib\tools.jar
 java -classpath "%CLASSPATH%;lib\ant.jar" org.apache.tools.ant.Main %1 %2 %3 %4 %5
