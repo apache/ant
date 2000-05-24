95ffb1ce494f158d2918345e1030b8a3c44a29c7
Arnout J. Kuiper <arnout@apache.org>
2000-05-24 14:35:22 +0000

    Two new tasks, untar & gunzip, to complement the tar & gzip.
    Submitted by:	Stefan Bodewig <stefan.bodewig@megabit.net>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@267671 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/org/apache/tools/ant/taskdefs/Untar.java
@@ -0,0 +1,145 @@
+/*
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 1999 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Tomcat", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+package org.apache.tools.ant.taskdefs;
+
+import org.apache.tools.ant.*;
+import org.apache.tools.tar.*;
+import java.io.*;
+/**
+ * Untar a file.
+ *
+ * Heavily based on the Expand task.
+ *
+ * @author Stefan Bodewig <a href="mailto:stefan.bodewig@megabit.net">stefan.bodewig@megabit.net</a>
+ */
+public class Untar extends Task {
+    private String dest; // req
+    private String source; // req
+
+    /**
+     * Do the work.
+     *
+     * @exception BuildException Thrown in unrecoverable error.
+     */
+    public void execute() throws BuildException {
+        try {
+            if (source == null) {
+                throw new BuildException("No source specified");
+            }
+            File srcF=project.resolveFile(source);
+            if (!srcF.exists()) {
+                throw new BuildException("source doesn't exist");
+            }
+
+            if (dest == null) {
+                throw new BuildException("No destination specified");
+            }
+
+            File dir=project.resolveFile(dest);
+
+            project.log("Expanding: " + srcF + " into " + dir, Project.MSG_INFO);
+            // code from WarExpand
+            TarInputStream tis = new TarInputStream(new FileInputStream(srcF));
+            TarEntry te = null;
+
+            while ((te = tis.getNextEntry()) != null) {
+                try {
+                    File f = new File(dir, project.translatePath(te.getName()));
+                    project.log("expand-file " + te.getName() , "untar", Project.MSG_VERBOSE );
+                    // create intermediary directories - sometimes tar don't add them
+                    File dirF=new File(f.getParent());
+                    dirF.mkdirs();
+
+                    if (te.isDirectory()) {
+                        f.mkdirs();
+                    } else {
+                        byte[] buffer = new byte[1024];
+                        int length = 0;
+                        FileOutputStream fos = new FileOutputStream(f);
+
+                        while ((length = tis.read(buffer)) >= 0) {
+                            fos.write(buffer, 0, length);
+                        }
+
+                        fos.close();
+                    }
+                } catch( FileNotFoundException ex ) {
+                    System.out.println("FileNotFoundException: " +  te.getName()  );
+                }
+            }
+        } catch (IOException ioe) {
+            throw new BuildException(ioe);
+        }
+    }
+
+    /**
+     * Set the destination directory. File will be untared into the
+     * destination directory.
+     *
+     * @param d Path to the directory.
+     */
+    public void setDest(String d) {
+        this.dest=d;
+    }
+
+    /**
+     * Set the path to tar-file.
+     *
+     * @param s Path to tar-file.
+     */
+    public void setSrc(String s) {
+        this.source = s;
+    }
+}
