961ad3a9d93808eb3dffdf1bb64fc5bf4d8d1fa6
Conor MacNeill <conor@apache.org>
2000-07-12 22:22:43 +0000

    Make sure all the pieces needed from bootstrap are present.
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@267773 13f79535-47bb-0310-9956-ffa450edef68

--- build.bat
+++ build.bat
@@ -3,10 +3,12 @@
 set REALANTHOME=%ANT_HOME%
 set ANT_HOME=.
 if not exist lib\ant.jar call bootstrap.bat
+if not exist bin\ant.bat call bootstrap.bat
+if not exist bin\lcp.bat call bootstrap.bat
 
 set ANT_INSTALL=
 if not "%REALANTHOME%" == "" set ANT_INSTALL=-Dant.install %REALANTHOME%
-call .\bin\ant %ANT_INSTALL% %1 %2 %3 %4 %5 %6 %7 %8 %9
+call .\bin\ant.bat %ANT_INSTALL% %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 rem clean up
 set ANT_HOME=%REALANTHOME%
