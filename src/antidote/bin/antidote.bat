dcba4b34366cde2807fe472c6a67bd68cf8afec6
metasim <dev-null@apache.org>
2000-11-14 18:47:52 +0000

    Removed Ant specific resource checking.
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@268182 13f79535-47bb-0310-9956-ffa450edef68

--- src/antidote/bin/antidote.bat
+++ src/antidote/bin/antidote.bat
@@ -36,31 +36,8 @@ goto end
 :checkJava
 if "%JAVACMD%" == "" set JAVACMD=java
 
-set LOCALCLASSPATH="%CLASSPATH%"
-for %%i in ("%ANT_HOME%\lib\*.jar") do call "%ANT_HOME%\bin\lcp.bat" "%%i"
-
-if "%JAVA_HOME%" == "" goto noJavaHome
-if exist "%JAVA_HOME%\lib\tools.jar" call "%ANT_HOME%\bin\lcp.bat" "%JAVA_HOME%\lib\tools.jar"
-if exist "%JAVA_HOME%\lib\classes.zip" call "%ANT_HOME%\bin\lcp.bat" "%JAVA_HOME%\lib\classes.zip"
-goto checkJikes
-
-:noJavaHome
-echo.
-echo Warning: JAVA_HOME environment variable is not set.
-echo   If build fails because sun.* classes could not be found
-echo   you will need to set the JAVA_HOME environment variable
-echo   to the installation directory of java.
-echo.
-
-:checkJikes
-if not "%JIKESPATH%" == "" goto runAntWithJikes
-
-:runAnt
-%JAVACMD% -classpath %LOCALCLASSPATH% -Dant.home="%ANT_HOME%" %ANT_OPTS% org.apache.tools.ant.Main %ANT_CMD_LINE_ARGS%
+%JAVACMD% -classpath %LOCALCLASSPATH% -Dant.home="%ANT_HOME%" org.apache.tools.ant.gui.Main %ANT_CMD_LINE_ARGS%
 goto end
-
-:runAntWithJikes
-%JAVACMD% -classpath %LOCALCLASSPATH% -Dant.home="%ANT_HOME%" -Djikes.class.path=%JIKESPATH% %ANT_OPTS% org.apache.tools.ant.gui.Main %ANT_CMD_LINE_ARGS%
 
 :end
 set LOCALCLASSPATH=
