d90a268ff36d9da9a40f66bcd764ae91bee174c3
Peter Donald <donaldp@apache.org>
2000-12-04 13:47:41 +0000

    oops - removing my funky elisp file.
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@268293 13f79535-47bb-0310-9956-ffa450edef68

--- proposal/myrmidon/emacs-jprj.el
+++
@@ -1,7 +0,0 @@
-(setq jprj-base-path (message "%s/" (expand-file-name (substitute-in-file-name "."))) )
-(setq jprj-src-path (message "%ssrc/java/" jprj-base-path) )
-(setq jprj-compile-command "./build.bat")
-(setq jprj-run-command "./build.bat&");; cd dist; bin/ant.bat -f ../src/make/sample.xmk&")
-;(setq tab-expansion-size 4)
-
-(load "update-prj")
