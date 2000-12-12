51e2cc543512d445e415bc5b7c2a42f98ddf8446
Peter Donald <donaldp@apache.org>
2000-12-12 11:51:11 +0000

    Added register-datatype task
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@268343 13f79535-47bb-0310-9956-ffa450edef68

--- proposal/myrmidon/src/java/org/apache/ant/tasklet/engine/DefaultTskDeployer.java
+++ proposal/myrmidon/src/java/org/apache/ant/tasklet/engine/DefaultTskDeployer.java
@@ -151,6 +151,27 @@ public class DefaultTskDeployer
     public void deployDataType( final String name, final String location, final URL url )
         throws DeploymentException
     {
+        checkDeployment( location, url );
+        final ZipFile zipFile = getZipFileFor( url );
+        final Configuration datatypedefs = loadConfiguration( zipFile, TSKDEF_FILE );
+        
+        try
+        {
+            final Iterator datatypes = datatypedefs.getChildren( "datatype" );
+            while( datatypes.hasNext() )
+            {
+                final Configuration datatype = (Configuration)datatypes.next();
+                if( datatype.getAttribute( "name" ).equals( name ) )
+                {
+                    handleDataType( datatype, url );
+                    break;
+                }
+            }
+        }
+        catch( final ConfigurationException ce )
+        {
+            throw new DeploymentException( "Malformed taskdefs.xml", ce );
+        }
     }
     
     public void deployTasklet( final String name, final String location, final URL url )
