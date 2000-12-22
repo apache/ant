736bd1b658a16317f90c94fea4e568867774fa48
Conor MacNeill <conor@apache.org>
2000-12-22 11:47:56 +0000

    A fun little task to play a sound at the end of a build. I got tired of waiting for some else to commit it. I made some small mods to Nick original proposal.
    Submitted by:	Nick Pellow <nick@cortexebusiness.com.au>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@268381 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/org/apache/tools/ant/taskdefs/optional/sound/SoundTask.java
@@ -0,0 +1,176 @@
+/*
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2000 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Tomcat", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+package org.apache.tools.ant.taskdefs.optional.sound;
+
+import org.apache.tools.ant.*;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * This is an example of an AntTask that makes of use of the AntSoundPlayer.
+ *
+ * There are four attributes to be set:
+ *
+ * <code>source</code>: the location of the audio file to be played
+ * <code>duration</code>: play the sound file continuously until "duration" milliseconds has expired
+ * <code>loops</code>: the number of times the sound file should be played until stopped
+ *
+ * I have only tested this with .WAV and .AIFF sound file formats. Both seem to work fine.
+ *
+ * plans for the future:
+ * - use the midi api to define sounds (or drum beat etc) in xml and have Ant play them back
+ *
+ * @author Nick Pellow
+ * @version $Revision$, $Date$
+ */
+
+public class SoundTask extends Task {
+
+    private BuildAlert  success = null;
+    private BuildAlert fail = null;
+
+    public BuildAlert createSuccess() {
+        success = new BuildAlert();
+        return success;
+    }
+
+    public BuildAlert createFail() {
+        fail = new BuildAlert();
+        return fail;
+     }
+
+    public SoundTask() {
+    }
+
+    public void init(){
+    }
+
+    public void execute() throws BuildException {
+        if ( success == null && fail == null) {
+            throw new BuildException("No nested elements provided.");
+        }
+
+        AntSoundPlayer soundPlayer = new AntSoundPlayer();
+        if (success != null) {
+            soundPlayer.addBuildSuccesfulSound(success.getSource(), success.getLoops(), success.getDuration());
+        }
+        
+        if (fail != null) {
+            soundPlayer.addBuildFailedSound(fail.getSource(), fail.getLoops(), fail.getDuration());
+        }
+        getProject().addBuildListener(soundPlayer);
+    }
+
+    /**
+     * A static class to be extended by any BuildAlert's that require the output of sound.
+     */
+    public static class BuildAlert {
+        private File file = null;
+        private int loops = 1;
+        private Long duration = null;
+
+        /**
+         * Sets the duration in milliseconds the file should be played.
+         */
+        public void setDuration(Long duration) {
+            this.duration = duration;
+        }
+
+        /**
+         * Sets the location of the file to get the audio.
+         *
+         * @param fileName the location of the audio file
+         */
+        public void setSource(File file) {
+            this.file = file;
+        }
+
+        /**
+         * This attribute sets the number of times the source file should
+         * be played.
+         *
+         * @param loops the number of loops to play the source file
+         */
+        public void setLoops(int loops) {
+            this.loops = loops;
+        }
+
+        /**
+         * Gets the duration in milliseconds the file should be played.
+         */
+        public Long getDuration() {
+            return this.duration;
+        }
+
+        /**
+         * Gets the location of the file to get the audio.
+         */
+        public File getSource() {
+            return this.file;
+        }
+
+        /**
+         * This attribute sets the number of times the source file should
+         * be played.
+         *
+         * @return the number of loops to play the source file
+         */
+        public int getLoops() {
+            return this.loops;
+        }
+    }
+}
+
