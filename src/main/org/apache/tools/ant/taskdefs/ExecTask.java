021634e2bbdf5da6d5b8e238a76685e1a2ead198
Peter Donald <donaldp@apache.org>
2001-01-21 00:41:54 +0000

    If directory is specified test if it's a valid one.
    Submitted By: "Nico Seessle" <Nico.Seessle@epost.de>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@268493 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/taskdefs/ExecTask.java
+++ src/main/org/apache/tools/ant/taskdefs/ExecTask.java
@@ -170,6 +170,12 @@ public class ExecTask extends Task {
         if (cmdl.getExecutable() == null) {
             throw new BuildException("no executable specified", location);
         }
+        if (dir != null && !dir.exists()) {
+        	throw new BuildException("The directory you specified does not exist");
+        }
+        if (dir != null && !dir.isDirectory()) {
+        	throw new BuildException("The directory you specified is not a directory");
+        }
     }
 
     /**
