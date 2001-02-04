65bbae5ae39fa78770da27fc165d38ac69966441
Peter Donald <donaldp@apache.org>
2001-02-04 00:51:59 +0000

    Made sure env vars are cleaned
    Also added bootstrap jars to CLASSPATh because for some reason they were not being added otherwise ???
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@268571 13f79535-47bb-0310-9956-ffa450edef68

--- build.bat
+++ build.bat
@@ -7,9 +7,10 @@ if exist bootstrap\lib\ant.jar if exist bootstrap\bin\ant.bat if exist bootstrap
 call bootstrap.bat
 
 :runAnt
-set LOCALCLASSPATH=lib\parser.jar;lib\jaxp.jar
+set LOCALCLASSPATH=lib\parser.jar;lib\jaxp.jar;bootstrap\lib\ant.jar
 for %%i in (lib\optional\*.jar) do call bootstrap\bin\lcp.bat "%%i"
 set CLASSPATH=%LOCALCLASSPATH%;%CLASSPATH%
+set LOCALCLASSPATH=
 
 if not "%REAL_ANT_HOME%" == "" goto install_ant
 call bootstrap\bin\ant.bat %1 %2 %3 %4 %5 %6 %7 %8 %9
