e94678ff499570c62ae2d6a99c775c4be981b0cf
Peter Donald <donaldp@apache.org>
2001-06-08 09:07:28 +0000

    Concrete task to define types - where user specifies type of defionition (ie task/data-type, aspect, listener etc).
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@269121 13f79535-47bb-0310-9956-ffa450edef68

---
+++ proposal/myrmidon/src/java/org/apache/myrmidon/libs/runtime/TypeDef.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included with this distribution in
+ * the LICENSE file.
+ */
+package org.apache.myrmidon.libs.runtime;
+
+import org.apache.myrmidon.api.Task;
+import org.apache.myrmidon.framework.AbstractTypeDef;
+
+/**
+ * Task to define a type.
+ *
+ * @author <a href="mailto:donaldp@apache.org">Peter Donald</a>
+ */
+public class TypeDef
+    extends AbstractTypeDef
+{
+    private String     m_type;
+
+    public void setType( final String type )
+    {
+        m_type = type;
+    }
+
+    protected String getTypeName()
+    {
+        return m_type;
+    }
+}
