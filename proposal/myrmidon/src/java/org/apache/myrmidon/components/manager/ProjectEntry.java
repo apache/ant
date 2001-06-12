087abe77e3929bc11311ae6f61999ce2bb798a24
Peter Donald <donaldp@apache.org>
2001-06-12 13:57:51 +0000

    Interface used to contain relevent data for each project managed by ProjectManager.
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@269162 13f79535-47bb-0310-9956-ffa450edef68

---
+++ proposal/myrmidon/src/java/org/apache/myrmidon/components/manager/ProjectEntry.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included with this distribution in
+ * the LICENSE file.
+ */
+package org.apache.myrmidon.components.manager;
+
+import java.util.ArrayList;
+import org.apache.myrmidon.components.model.Project;
+import org.apache.myrmidon.components.executor.ExecutionFrame;
+
+/**
+ * This contains detaisl for each project that is managed by ProjectManager.
+ *
+ * @author <a href="mailto:donaldp@apache.org">Peter Donald</a>
+ */
+public final class ProjectEntry
+{
+    private final Project         m_project;
+    private final ExecutionFrame  m_frame;
+    private final ArrayList       m_targetsCompleted   = new ArrayList();
+
+    public ProjectEntry( final Project project, 
+                         final ExecutionFrame frame )
+    {
+        m_project = project;
+        m_frame = frame;
+    }
+
+    public Project getProject()
+    {
+        return m_project;
+    }
+
+    public ExecutionFrame getFrame()
+    {
+        return m_frame;
+    }
+
+    public boolean isTargetCompleted( final String target )
+    {
+        return m_targetsCompleted.contains( target );
+    }
+
+    public void completeTarget( final String target )
+    {
+        m_targetsCompleted.add( target );
+    }
+}
