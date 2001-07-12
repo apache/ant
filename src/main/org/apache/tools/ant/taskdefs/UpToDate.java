5bb077f59871d3c25031bf9546bcc42e5331de33
Stefan Bodewig <bodewig@apache.org>
2001-07-12 10:06:40 +0000

    make <uptodate> search for target files in the same directory as source files
    Submitted by:	Gunnlaugur Thor Briem <gthb@dimon.is>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@269326 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/taskdefs/UpToDate.java
+++ src/main/org/apache/tools/ant/taskdefs/UpToDate.java
@@ -177,13 +177,15 @@ public class UpToDate extends MatchingTask {
     protected boolean scanDir(File srcDir, String files[]) {
         SourceFileScanner sfs = new SourceFileScanner(this);
         FileNameMapper mapper = null;
+        File dir = srcDir;
         if (mapperElement == null) {
             MergingMapper mm = new MergingMapper();
             mm.setTo(_targetFile.getAbsolutePath());
             mapper = mm;
+            dir = null;
         } else {
             mapper = mapperElement.getImplementation();
         }
-        return sfs.restrict(files, srcDir, null, mapper).length == 0;
+        return sfs.restrict(files, srcDir, dir, mapper).length == 0;
     }
 }
