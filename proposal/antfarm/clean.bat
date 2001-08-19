596b21c73022c953121a97f8d4f51c15a4602bbe
No Author <dev-null@apache.org>
2001-08-19 05:52:00 +0000

    This commit was manufactured by cvs2svn to create tag 'ANT_MAIN_14B2'.
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/tags/ANT_MAIN_14B2@269591 13f79535-47bb-0310-9956-ffa450edef68

--- proposal/antfarm/clean.bat
+++
@@ -1,3 +0,0 @@
-if exist boot rmdir /s /q boot
-if exist temp rmdir /s /q temp
-if exist dist rmdir /s /q dist
--- proposal/antfarm/core/ant.java
+++
@@ -1,9 +0,0 @@
-/* Copyright (c) 2000 The Apache Software Foundation */
-
-import org.apache.tools.ant.cmdline.Main;
-
-public class ant {
-    public static void main(String[] args) throws Exception {
-        Main.main(args);
-    }
-}
