bad4737cec9376354a5166f27bcdf778ded8d415
Conor MacNeill <conor@apache.org>
2001-09-13 13:06:51 +0000

    Use the task's idea of its name rather than trying to extract it from the class which is not really relevant to the user
    PR:	3393
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/branches/ANT_14_BRANCH@269703 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/XmlLogger.java
+++ src/main/org/apache/tools/ant/XmlLogger.java
@@ -200,11 +200,7 @@ public class XmlLogger implements BuildListener {
         taskElement.startTime = System.currentTimeMillis();
         taskElement.element = doc.createElement(TASK_TAG);
         
-        String name = task.getClass().getName();
-        int pos = name.lastIndexOf(".");
-        if (pos != -1) {
-            name = name.substring(pos + 1);
-        }
+        String name = event.getTask().getTaskName();
         taskElement.element.setAttribute(NAME_ATTR, name);
         taskElement.element.setAttribute(LOCATION_ATTR, event.getTask().getLocation().toString());
         tasks.put(task, taskElement);
