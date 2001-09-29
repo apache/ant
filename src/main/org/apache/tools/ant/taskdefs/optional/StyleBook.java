74664ce5dd029bd2b405d78aa841467e4582ed66
Peter Donald <donaldp@apache.org>
2001-09-29 03:45:56 +0000

    Added the ability to set the loader-file as a parameter
    Submitted by: Marcus B�rger <Marcus.Boerger@post.rwth-aachen.de>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@269742 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/taskdefs/optional/StyleBook.java
+++ src/main/org/apache/tools/ant/taskdefs/optional/StyleBook.java
@@ -53,23 +53,25 @@
  */
 package org.apache.tools.ant.taskdefs.optional;
 
-import java.io.File; 
-import org.apache.tools.ant.BuildException; 
-import org.apache.tools.ant.types.CommandlineJava; 
-import org.apache.tools.ant.types.Path; 
-import org.apache.tools.ant.Task; 
-import org.apache.tools.ant.taskdefs.Java; 
+import java.io.File;
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.types.CommandlineJava;
+import org.apache.tools.ant.types.Path;
+import org.apache.tools.ant.Task;
+import org.apache.tools.ant.taskdefs.Java;
 
-/** 
+/**
  * Basic task for apache stylebook.
- * 
- * @author <a href="mailto:donaldp@apache.org">Peter Donald</a> 
- */ 
-public class StyleBook 
-    extends Java 
-{ 
+ *
+ * @author <a href="mailto:donaldp@apache.org">Peter Donald</a>
+ * @author <a href="mailto:marcus.boerger@post.rwth-aachen.de">Marcus B&ouml;rger</a>
+ */
+public class StyleBook
+    extends Java
+{
     protected File                   m_targetDirectory;
     protected File                   m_skinDirectory;
+    protected String                 m_loaderConfig;
     protected File                   m_book;
 
     public StyleBook() {
@@ -80,18 +82,22 @@ public class StyleBook
 
     public void setBook( final File book ) {
         m_book = book;
-    } 
- 
+    }
+
     public void setSkinDirectory( final File skinDirectory ) {
         m_skinDirectory = skinDirectory;
-    } 
+    }
 
     public void setTargetDirectory( final File targetDirectory ) {
         m_targetDirectory = targetDirectory;
-    } 
-    
-    public void execute()  
-        throws BuildException  { 
+    }
+
+    public void setLoaderConfig( final String loaderConfig ) {
+        m_loaderConfig = loaderConfig;
+    }
+
+    public void execute()
+        throws BuildException  {
 
         if( null == m_targetDirectory ) {
             throw new BuildException( "TargetDirectory attribute not set." );
@@ -103,14 +109,16 @@ public class StyleBook
 
         if( null == m_book ) {
             throw new BuildException( "book attribute not set." );
-        } 
+        }
 
         createArg().setValue( "targetDirectory=" + m_targetDirectory );
         createArg().setValue( m_book.toString() );
         createArg().setValue( m_skinDirectory.toString() );
+        if( null != m_loaderConfig ) {
+          createArg().setValue( "loaderConfig=" + m_loaderConfig );
+        }
 
         super.execute();
-    } 
-} 
- 
- 
+    }
+}
+
