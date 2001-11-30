f6db0835f7dad339a60c73c0aab595657e72e327
Stefan Bodewig <bodewig@apache.org>
2001-11-30 12:24:17 +0000

    New task <input>
    Submitted by:	Ulrich Schmidt <ulrich.schmidt@gmx.net>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@270051 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/org/apache/tools/ant/taskdefs/Input.java
@@ -0,0 +1,212 @@
+/*
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2001 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Ant", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+package org.apache.tools.ant.taskdefs;
+
+import  java.io.BufferedReader;
+import  java.io.InputStreamReader;
+import  java.io.IOException;
+import  java.util.StringTokenizer;
+import  java.util.Vector;
+import  org.apache.tools.ant.*;
+
+
+/**
+ * Ant task to read input line from console.
+ *
+ * @author Ulrich Schmidt <usch@usch.net>
+ */
+public class Input extends Task {
+    private String validargs = null;
+    private String exitargs = null;
+    private String exitmessage = "Build abborted.";
+    private String message = "";
+    private String addproperty = null;
+    private String input = null;
+
+    /**
+     * Defines valid input parameters as comma separated String. If set, input
+     * task will reject any input not defined as accepted and requires the user
+     * to reenter it. Validargs are case sensitive. If you want 'a' and 'A' to
+     * be accepted you need to define both values as accepted arguments.
+     *
+     * @param validargs A comma separated String defining valid input args.
+     */
+    public void setValidargs (String validargs) {
+        this.validargs = validargs;
+    }
+
+    /**
+     * Defines the name of a property to be created from input. Behaviour is
+     * according to property task which means that existing properties
+     * cannot be overriden.
+     *
+     * @param exitargs Name for the property to be created from input
+     */
+    public void setAddproperty (String addproperty) {
+        this.addproperty = addproperty;
+    }
+
+    /*
+     * Defines exit condition parameters as comma separated String. If input
+     * matches one of these input task will end build by throwing a
+     * BuildException. ExitArgs are case sensitive. If you want the build to
+     * end on 'x' and 'X' you need to define both values as exit arguments.
+     *
+     * @param exitargs A comma separated String defining exit arguments.
+     */
+    public void setExitargs (String exitargs) {
+        this.exitargs = exitargs;
+    }
+
+    /**
+     * Sets the ExitMessage which gets displayed when exiting the build run.
+     * Default is 'Build abborted.'
+     * @param exitmessage The exit message to be displayed.
+     */
+    public void setExitmessage (String exitmessage) {
+        this.exitmessage = exitmessage;
+    }
+
+    /**
+     * Sets the Message which gets displayed to the user during the build run.
+     * @param message The message to be displayed.
+     */
+    public void setMessage (String message) {
+        this.message = message;
+    }
+
+    /**
+     * Sets surrogate input to allow automated testing.
+     * @param input The surrogate input used for testing.
+     */
+    public void setTestinput (String testinput) {
+        this.input = testinput;
+    }
+
+    /**
+     * No arg constructor.
+     */
+    public Input () {
+    }
+
+    /**
+     * Actual test method executed by jakarta-ant.
+     * @exception BuildException
+     */
+    public void execute () throws BuildException {
+        Vector accept = null;
+        if (validargs != null) {
+            accept = new Vector();
+            StringTokenizer stok = new StringTokenizer(validargs, ",", false);
+            while (stok.hasMoreTokens()) {
+                accept.addElement(stok.nextToken());
+            }
+        }
+        Vector exit = null;
+        if (exitargs != null) {
+            exit = new Vector();
+            StringTokenizer stok = new StringTokenizer(exitargs, ",", false);
+            while (stok.hasMoreTokens()) {
+                exit.addElement(stok.nextToken());
+            }
+        }
+        log(message, Project.MSG_WARN);
+        if (input == null) {
+            try {
+                BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+                input = in.readLine();
+                if (accept != null) {
+                    while (!accept.contains(input)) {
+                        log(message, Project.MSG_WARN);
+                        input = in.readLine();
+                    }
+                }
+            } catch (IOException e) {
+                throw  new BuildException("Failed to read input from Console.", e);
+            }
+        }
+        // not quite the original intention of this task but for the sake
+        // of testing ;-)
+        else {
+            if (accept != null && (!accept.contains(input))) {
+                throw  new BuildException("Invalid input please reenter.");
+            }
+        }
+        // adopted from org.apache.tools.ant.taskdefs.Property
+        if (addproperty != null) {
+            if (project.getProperty(addproperty) == null) {
+                project.setProperty(addproperty, input);
+            }
+            else {
+                log("Override ignored for " + addproperty, Project.MSG_VERBOSE);
+            }
+        }
+        if (exit != null && exit.contains(input)) {
+            throw  new BuildException(exitmessage);
+        }
+    }
+
+    // copied n' pasted from org.apache.tools.ant.taskdefs.Exit
+    /**
+     * Set a multiline message.
+     */
+    public void addText(String msg) {
+        message +=
+            ProjectHelper.replaceProperties(project, msg, project.getProperties());
+    }
+}
+
+
+
---
+++ src/testcases/org/apache/tools/ant/taskdefs/InputTest.java
@@ -0,0 +1,118 @@
+/*
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2001 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Ant", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+package org.apache.tools.ant.taskdefs;
+import org.apache.tools.ant.BuildFileTest;
+
+/**
+ * @author Ulrich Schmidt <usch@usch.net>
+ */
+public class InputTest extends BuildFileTest {
+
+    public InputTest(String name) {
+        super(name);
+    }
+
+    public void setUp() {
+        configureProject("src/etc/testcases/taskdefs/input.xml");
+    }
+
+    public void test1() {
+        expectLog("test1", "Press Return key to continue...");
+    }
+
+    public void test2() {
+        expectLog("test2", "Press Return key to continue...");
+    }
+
+    public void test3() {
+        String log = "All data is going to be deleted from DB continue (y/n)?";
+        String message = "Invalid input please reenter.";
+        try {
+            executeTarget("test3");
+        } catch (org.apache.tools.ant.BuildException e) {
+            String realLog = getLog();
+            assertEquals(log, realLog);
+            assertEquals(message, e.getMessage());
+        }
+    }
+
+    public void test4() {
+        String log = "All data is going to be deleted from DB continue (y/n)?";
+        String message = "Build abborted.";
+        try {
+            executeTarget("test4");
+        } catch (org.apache.tools.ant.BuildException e) {
+            String realLog = getLog();
+            assertEquals(log, realLog);
+            assertEquals(message, e.getMessage());
+        }
+    }
+
+    public void test5() {
+        expectLog("test5",
+                "All data is going to be deleted from DB continue (y/n)?");
+    }
+
+    public void test6() {
+        expectLog("test6",
+                "Please enter db-username:");
+        assertEquals("scott", project.getProperty("db.user"));
+    }
+
+    public void test7() {
+        expectBuildException("test7",
+                "Don't trust if you don't have the source ;-)");
+    }
+}
