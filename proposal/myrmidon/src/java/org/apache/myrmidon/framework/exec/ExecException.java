4c36f7cb5aca402d90ea046454939860ba5457e8
Peter Donald <donaldp@apache.org>
2001-12-22 23:54:18 +0000

    Add an exception that indicates an error running a native process.
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@270334 13f79535-47bb-0310-9956-ffa450edef68

---
+++ proposal/myrmidon/src/java/org/apache/myrmidon/framework/exec/ExecException.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included with this distribution in
+ * the LICENSE file.
+ */
+package org.apache.myrmidon.framework.exec;
+
+import org.apache.avalon.framework.CascadingException;
+
+/**
+ * ExecException indicates there was an error executing native process.
+ *
+ * @author <a href="mailto:peter@apache.org">Peter Donald</a>
+ */
+public class ExecException
+    extends CascadingException
+{
+    /**
+     * Basic constructor for exception that does not specify a message
+     */
+    public ExecException()
+    {
+        this( "", null );
+    }
+
+    /**
+     * Basic constructor with a message
+     *
+     * @param message the message
+     */
+    public ExecException( final String message )
+    {
+        this( message, null );
+    }
+
+    /**
+     * Constructor that builds cascade so that other exception information can be retained.
+     *
+     * @param message the message
+     * @param throwable the throwable
+     */
+    public ExecException( final String message, final Throwable throwable )
+    {
+        super( message, throwable );
+    }
+}
+
