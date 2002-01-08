437a1cfc7a722a0421683c5720a9856e583f65f4
Stephane Bailliez <sbailliez@apache.org>
2002-01-08 23:59:33 +0000

    Remove double check for same condition.
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@270642 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/taskdefs/optional/perforce/P4Reopen.java
+++ src/main/org/apache/tools/ant/taskdefs/optional/perforce/P4Reopen.java
@@ -78,9 +78,7 @@ public class P4Reopen extends P4Base {
 
     public void execute() throws BuildException {
         if(P4View == null) {
-          if(P4View == null) {
             throw new BuildException("No view specified to reopen");
-          }
         }
         execP4Command("-s reopen -c "+toChange+" "+P4View, new SimpleP4OutputHandler(this));
     }
