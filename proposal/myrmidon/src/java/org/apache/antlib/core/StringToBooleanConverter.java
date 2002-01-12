465dd5dfe1c59094fa0418d949853f54d53fa181
Peter Donald <donaldp@apache.org>
2002-01-12 05:49:17 +0000

    Add a converter for booleans
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@270685 13f79535-47bb-0310-9956-ffa450edef68

---
+++ proposal/myrmidon/src/java/org/apache/antlib/core/StringToBooleanConverter.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
+package org.apache.antlib.core;
+
+import org.apache.avalon.excalibur.i18n.ResourceManager;
+import org.apache.avalon.excalibur.i18n.Resources;
+import org.apache.avalon.framework.context.Context;
+import org.apache.myrmidon.converter.AbstractConverter;
+import org.apache.myrmidon.converter.ConverterException;
+
+/**
+ * String to boolean converter
+ *
+ * @author <a href="mailto:peter@apache.org">Peter Donald</a>
+ */
+public class StringToBooleanConverter
+    extends AbstractConverter
+{
+    private static final Resources REZ =
+        ResourceManager.getPackageResources( StringToBooleanConverter.class );
+
+    public StringToBooleanConverter()
+    {
+        super( String.class, Boolean.class );
+    }
+
+    public Object convert( final Object object, final Context context )
+        throws ConverterException
+    {
+        final String string = (String)object;
+        if( string.equals( "true" ) )
+        {
+            return Boolean.TRUE;
+        }
+        else if( string.equals( "false" ) )
+        {
+            return Boolean.FALSE;
+        }
+        else
+        {
+            final String message = REZ.getString( "convert.bad-boolean.error", object );
+            throw new ConverterException( message );
+        }
+    }
+}
+
