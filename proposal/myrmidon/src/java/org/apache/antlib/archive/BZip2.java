1e745fe39610746a046ce2ea1eef71fa15a6ea8e
Peter Donald <donaldp@apache.org>
2002-01-20 02:10:27 +0000

    Update location of BZip stuff
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@270796 13f79535-47bb-0310-9956-ffa450edef68

--- proposal/myrmidon/src/java/org/apache/antlib/archive/BZip2.java
+++ proposal/myrmidon/src/java/org/apache/antlib/archive/BZip2.java
@@ -10,7 +10,7 @@ package org.apache.antlib.archive;
 import java.io.IOException;
 import java.io.OutputStream;
 import org.apache.myrmidon.api.TaskException;
-import org.apache.tools.bzip2.CBZip2OutputStream;
+import org.apache.aut.bzip2.CBZip2OutputStream;
 
 /**
  * Compresses a file with the BZip2 algorithm. Normally used to compress
