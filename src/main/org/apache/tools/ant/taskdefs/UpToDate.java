46b571c78c4c88ac9285d0de02de0073e7631642
Stephane Bailliez <sbailliez@apache.org>
2002-01-26 19:36:26 +0000

    Fix array declarators
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@270904 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/taskdefs/UpToDate.java
+++ src/main/org/apache/tools/ant/taskdefs/UpToDate.java
@@ -1,7 +1,7 @@
 /*
  * The Apache Software License, Version 1.1
  *
- * Copyright (c) 2000-2001 The Apache Software Foundation.  All rights
+ * Copyright (c) 2000-2002 The Apache Software Foundation.  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -189,7 +189,7 @@ public class UpToDate extends MatchingTask implements Condition {
         }
     }
 
-    protected boolean scanDir(File srcDir, String files[]) {
+    protected boolean scanDir(File srcDir, String[] files) {
         SourceFileScanner sfs = new SourceFileScanner(this);
         FileNameMapper mapper = null;
         File dir = srcDir;
