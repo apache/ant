1b022c8fbe8e3cb042b0d1eac1b5a6c3350c6bf6
Peter Donald <donaldp@apache.org>
2002-01-26 12:10:47 +0000

    Move []s to component type and not postfix variable (like icky c programmers do)
    Removing a Audit violation
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@270884 13f79535-47bb-0310-9956-ffa450edef68

--- proposal/myrmidon/src/java/org/apache/aut/bzip2/BZip2Constants.java
+++ proposal/myrmidon/src/java/org/apache/aut/bzip2/BZip2Constants.java
@@ -26,7 +26,7 @@ interface BZip2Constants
     int MAX_SELECTORS = ( 2 + ( 900000 / G_SIZE ) );
     int NUM_OVERSHOOT_BYTES = 20;
 
-    int RAND_NUMS[] = new int[]
+    int[] RAND_NUMS = new int[]
     {
         619, 720, 127, 481, 931, 816, 813, 233, 566, 247,
         985, 724, 205, 454, 863, 491, 741, 242, 949, 214,
--- proposal/myrmidon/src/java/org/apache/aut/bzip2/CRC.java
+++ proposal/myrmidon/src/java/org/apache/aut/bzip2/CRC.java
@@ -15,7 +15,7 @@ package org.apache.aut.bzip2;
  */
 class CRC
 {
-    private static int CRC32_TABLE[] = new int[]
+    private static int[] CRC32_TABLE = new int[]
     {
         0x00000000, 0x04c11db7, 0x09823b6e, 0x0d4326d9,
         0x130476dc, 0x17c56b6b, 0x1a864db2, 0x1e475005,
