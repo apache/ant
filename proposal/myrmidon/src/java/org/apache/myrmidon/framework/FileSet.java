e29065397c364728264e97982d7cf9b21f8ad00c
Peter Donald <donaldp@apache.org>
2002-01-27 09:37:06 +0000

    Add a basic class for fileset
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@270935 13f79535-47bb-0310-9956-ffa450edef68

---
+++ proposal/myrmidon/src/java/org/apache/myrmidon/framework/FileSet.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included  with this distribution in
+ * the LICENSE.txt file.
+ */
+package org.apache.myrmidon.framework;
+
+import java.io.File;
+
+/**
+ * A FileSet represents a set of files selected by patterns with a
+ * specified root.
+ *
+ * @author <a href="mailto:peter@apache.org">Peter Donald</a>
+ * @version $Revision$ $Date$
+ */
+public class FileSet
+    extends AbstractFileSet
+{
+    private File m_dir;
+
+    /**
+     * Specify the base directory at which the file set is rooted.
+     */
+    public final void setDir( File dir )
+    {
+        m_dir = dir;
+    }
+
+    public final File getDir()
+    {
+        return m_dir;
+    }
+}
