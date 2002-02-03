26bfd9b1ecdbacca538de9b864c585a18f67de1d
Magesh Umasankar <umagesh@apache.org>
2002-02-03 01:53:34 +0000

    The pattern definition **/._* has been added to the default excludes list.
    PR: 5886
    
    Requested by: gerti-apache@bitart.com (Gerd Knops)
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@271097 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/DirectoryScanner.java
+++ src/main/org/apache/tools/ant/DirectoryScanner.java
@@ -1,7 +1,7 @@
 /*
  * The Apache Software License, Version 1.1
  *
- * Copyright (c) 2000-2001 The Apache Software Foundation.  All rights
+ * Copyright (c) 2000-2002 The Apache Software Foundation.  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -151,6 +151,7 @@ public class DirectoryScanner implements FileScanner {
         "**/#*#",
         "**/.#*",
         "**/%*%",
+        "**/._*",
         "**/CVS",
         "**/CVS/**",
         "**/.cvsignore",
