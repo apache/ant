8c68d4864950b4f5fa519753d19301cf84bf91bd
Stephane Bailliez <sbailliez@apache.org>
2002-02-11 22:47:21 +0000

    Make sure you do not compare apples to oranges
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@271286 13f79535-47bb-0310-9956-ffa450edef68

--- proposal/sandbox/junit/src/main/org/apache/tools/ant/taskdefs/optional/rjunit/formatter/XMLFormatter.java
+++ proposal/sandbox/junit/src/main/org/apache/tools/ant/taskdefs/optional/rjunit/formatter/XMLFormatter.java
@@ -211,7 +211,9 @@ public class XMLFormatter extends BaseStreamFormatter {
         String name = JUnitHelper.getTestName(evt.getName());
         test.setAttribute(ATTR_NAME, name);
         String suiteName = JUnitHelper.getSuiteName(evt.getName());
-        if ( !suiteName.equals(lastSuiteElement.getAttribute("name")) ){
+        String lastSuiteName = lastSuiteElement.getAttribute(ATTR_PACKAGE)
+                + "." + lastSuiteElement.getAttribute(ATTR_NAME);
+        if ( !suiteName.equals(lastSuiteName) ){
             throw new BuildException("Received testcase from test "
                     + suiteName + " and was expecting "
                     + lastSuiteElement.getAttribute("name"));
