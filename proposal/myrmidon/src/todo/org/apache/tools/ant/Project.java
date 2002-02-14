dceffe022efa9db433ceeb29f444f1995f38061e
Peter Donald <donaldp@apache.org>
2002-02-14 09:40:39 +0000

    Remove some more cruft
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@271323 13f79535-47bb-0310-9956-ffa450edef68

--- proposal/myrmidon/src/todo/org/apache/tools/ant/Project.java
+++ proposal/myrmidon/src/todo/org/apache/tools/ant/Project.java
@@ -7,11 +7,8 @@
  */
 package org.apache.tools.ant;
 
-import java.io.File;
 import java.util.Enumeration;
 import java.util.Hashtable;
-import org.apache.myrmidon.api.TaskException;
-import org.apache.myrmidon.listeners.ProjectListener;
 import org.apache.tools.ant.types.FilterSet;
 
 /**
@@ -27,12 +24,6 @@ import org.apache.tools.ant.types.FilterSet;
  */
 public class Project
 {
-    public final static int MSG_ERR = 0;
-    public final static int MSG_WARN = 1;
-    public final static int MSG_INFO = 2;
-    public final static int MSG_VERBOSE = 3;
-    public final static int MSG_DEBUG = 4;
-
     private Hashtable properties = new Hashtable();
     private FilterSet globalFilterSet = new FilterSet();
 
@@ -40,28 +31,6 @@ public class Project
      * Records the latest task on a thread
      */
     private Hashtable threadTasks = new Hashtable();
-    private File baseDir;
-
-    /**
-     * get the base directory of the project as a file object
-     *
-     * @return the base directory. If this is null, then the base dir is not
-     *      valid
-     */
-    public File getBaseDir()
-    {
-        return baseDir;
-    }
-
-    /**
-     * get the current task definition hashtable
-     *
-     * @return The DataTypeDefinitions value
-     */
-    public Hashtable getDataTypeDefinitions()
-    {
-        return null;
-    }
 
     public FilterSet getGlobalFilterSet()
     {
@@ -88,58 +57,6 @@ public class Project
         return propertiesCopy;
     }
 
-    /**
-     * query a property.
-     *
-     * @param name the name of the property
-     * @return the property value, or null for no match
-     */
-    public String getProperty( String name )
-    {
-        if( name == null )
-        {
-            return null;
-        }
-        String property = (String)properties.get( name );
-        return property;
-    }
-
-    /**
-     * @param key Description of Parameter
-     * @return The object with the "id" key.
-     */
-    public Object getReference( String key )
-    {
-        return null;
-    }
-
-    /**
-     * get the current task definition hashtable
-     *
-     * @return The TaskDefinitions value
-     */
-    public Hashtable getTaskDefinitions()
-    {
-        return null;
-    }
-
-    public void addProjectListener( final ProjectListener listener )
-    {
-    }
-
-    /**
-     * create a new task instance
-     *
-     * @param taskType name of the task
-     * @return null if the task name is unknown
-     * @throws TaskException when task creation goes bad
-     */
-    public Task createTask( String taskType )
-        throws TaskException
-    {
-        throw new TaskException( "Task needs reimplementing" );
-    }
-
     public void demuxOutput( String line, boolean isError )
     {
         Task task = (Task)threadTasks.get( Thread.currentThread() );
@@ -158,30 +75,5 @@ public class Project
                 task.handleOutput( line );
             }
         }
-    }
-
-    /**
-     * Output a message to the log with the given log level and an event scope
-     * of project
-     *
-     * @param msg text to log
-     * @param msgLevel level to log at
-     */
-    public void log( String msg, int msgLevel )
-    {
-    }
-
-    /**
-     * Replace ${} style constructions in the given value with the string value
-     * of the corresponding data types.
-     *
-     * @param value the string to be scanned for property references.
-     * @return Description of the Returned Value
-     * @exception TaskException Description of Exception
-     */
-    public String replaceProperties( String value )
-        throws TaskException
-    {
-        return null;
     }
 }
