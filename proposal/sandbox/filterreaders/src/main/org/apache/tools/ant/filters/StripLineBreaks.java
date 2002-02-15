8b00ce3f42610084921921e60a77473199eb0d2e
Magesh Umasankar <umagesh@apache.org>
2002-02-15 21:50:55 +0000

    Initial version of FilterReaderSet implementation with <loadfile> as example.
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@271370 13f79535-47bb-0310-9956-ffa450edef68

---
+++ proposal/sandbox/filterreaders/src/main/org/apache/tools/ant/filters/StripLineBreaks.java
@@ -0,0 +1,101 @@
+package org.apache.tools.ant.filters;
+
+import java.io.FilterReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Hashtable;
+
+import org.apache.tools.ant.types.Parameterizable;
+
+/**
+ * Filter to flatten the stream to a single line.
+ *
+ * @author Steve Loughran
+ * @author <a href="mailto:umagesh@apache.org">Magesh Umasankar</a>
+ */
+public final class StripLineBreaks
+    extends FilterReader
+    implements Parameterizable
+{
+    /**
+     * Linebreaks. What do to on funny IBM mainframes with odd line endings?
+     */
+    private static final String DEFAULT_LINE_BREAKS = "\r\n";
+
+    /**
+     * Linebreaks key that can be set via param element of
+     * AntFilterReader
+     */
+    private static final String LINE_BREAKS_KEY = "linebreaks";
+
+    private Hashtable parameters;
+
+    /**
+     * Create a new filtered reader.
+     *
+     * @param in  a Reader object providing the underlying stream.
+     */
+    public StripLineBreaks(final Reader in) {
+        super(in);
+    }
+
+    /**
+     * Strip line break characters from an array.
+     *
+     * @exception  IOException  If an I/O error occurs
+     */
+    public final int read(final char[] cbuf) throws IOException {
+        int length = -1;
+        if (cbuf != null) {
+            length = cbuf.length;
+            if (in != null) {
+                length = in.read(cbuf);
+            }
+            if (length != -1) {
+                String str = new String(cbuf, 0, length);
+                str = stripLineBreaks(str);
+                final char[] newcbuf = str.toCharArray();
+                System.arraycopy(newcbuf, 0, cbuf, 0, newcbuf.length);
+                for (int j = newcbuf.length; j < cbuf.length; j++) {
+                    cbuf[j] = 0;
+                }
+                length = newcbuf.length;
+            }
+        }
+        return length;
+    }
+
+    /**
+     * strip out all line breaks from a string.
+     * @param source source
+     * This implementation always duplicates the string; it is nominally possible to probe
+     * the string first looking for any line breaks before bothering to do a copy. But we assume if
+     * the option is requested, then line breaks are probably in the source string.
+     */
+    private final String stripLineBreaks(final String source) {
+        final int len=source.length();
+        final String userDefinedLineBreaks =
+            (String) parameters.get(LINE_BREAKS_KEY);
+
+        String lineBreaks = DEFAULT_LINE_BREAKS;
+        if (userDefinedLineBreaks != null) {
+            lineBreaks = userDefinedLineBreaks;
+        }
+        final StringBuffer dest=new StringBuffer(len);
+        for(int i=0;i<len;++i) {
+            final char ch=source.charAt(i);
+            if(lineBreaks.indexOf(ch)==-1) {
+                dest.append(ch);
+            }
+        }
+        return new String(dest);
+
+    }
+
+    /**
+     * Set Parameters
+     */
+    public final void setParameters(final Hashtable parameters) {
+        this.parameters = parameters;
+    }
+}
---
+++ proposal/sandbox/filterreaders/src/main/org/apache/tools/ant/taskdefs/LoadFile.java
@@ -0,0 +1,291 @@
+/*
+ *  The Apache Software License, Version 1.1
+ *
+ *  Copyright (c) 2001-2002 The Apache Software Foundation.  All rights
+ *  reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright
+ *  notice, this list of conditions and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *  notice, this list of conditions and the following disclaimer in
+ *  the documentation and/or other materials provided with the
+ *  distribution.
+ *
+ *  3. The end-user documentation included with the redistribution, if
+ *  any, must include the following acknowlegement:
+ *  "This product includes software developed by the
+ *  Apache Software Foundation (http://www.apache.org/)."
+ *  Alternately, this acknowlegement may appear in the software itself,
+ *  if and wherever such third-party acknowlegements normally appear.
+ *
+ *  4. The names "The Jakarta Project", "Ant", and "Apache Software
+ *  Foundation" must not be used to endorse or promote products derived
+ *  from this software without prior written permission. For written
+ *  permission, please contact apache@apache.org.
+ *
+ *  5. Products derived from this software may not be called "Apache"
+ *  nor may "Apache" appear in their names without prior written
+ *  permission of the Apache Group.
+ *
+ *  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ *  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ *  ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ *  USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ *  OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ *  SUCH DAMAGE.
+ *  ====================================================================
+ *
+ *  This software consists of voluntary contributions made by many
+ *  individuals on behalf of the Apache Software Foundation.  For more
+ *  information on the Apache Software Foundation, please see
+ *  <http://www.apache.org/>.
+ */
+package org.apache.tools.ant.taskdefs;
+
+import org.apache.tools.ant.Task;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.types.AntFilterReader;
+import org.apache.tools.ant.types.FilterReaderSet;
+import org.apache.tools.ant.types.Parameterizable;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.io.*;
+import java.util.Hashtable;
+import java.util.Vector;
+
+/**
+ * Load a file into a property
+ *
+ * @author Steve Loughran
+ * @author <a href="mailto:umagesh@apache.org">Magesh Umasankar</a>
+ * @created 10 December 2001
+ */
+public class LoadFile extends Task {
+
+    /**
+     * source file, usually null
+     */
+    private File srcFile = null;
+
+    /**
+     * what to do when it goes pear-shaped
+     */
+    private boolean failOnError = true;
+
+    /**
+     * Encoding to use for filenames, defaults to the platform's default
+     * encoding.
+     */
+    private String encoding = null;
+
+    /**
+     * name of property
+     */
+    private String property = null;
+
+    /**
+     * flag to control whether props get evaluated or not
+     */
+    private boolean evaluateProperties=false;
+
+    /**
+     * Holds filterReaderSets
+     */
+    private Vector filterReaderSets=new Vector();
+
+    /**
+     * Encoding to use for filenames, defaults to the platform's default
+     * encoding. <p>
+     *
+     * For a list of possible values see <a href="http://java.sun.com/products/jdk/1.2/docs/guide/internat/encoding.doc.html">
+     * http://java.sun.com/products/jdk/1.2/docs/guide/internat/encoding.doc.html
+     * </a>.</p>
+     *
+     * @param encoding The new Encoding value
+     */
+
+    public void setEncoding(String encoding) {
+        this.encoding = encoding;
+    }
+
+
+    /**
+     * Sets the Property attribute of the LoadFile object
+     *
+     * @param property The new Property value
+     */
+    public void setProperty(String property) {
+        this.property = property;
+    }
+
+
+    /**
+     * Sets the srcfile attribute.
+     *
+     * @param srcFile The new SrcFile value
+     */
+    public void setSrcFile(File srcFile) {
+        this.srcFile = srcFile;
+    }
+
+
+    /**
+     * Sets the Failonerror attribute of the LoadFile object
+     *
+     * @param fail The new Failonerror value
+     */
+    public void setFailonerror(boolean fail) {
+        failOnError = fail;
+    }
+
+
+    /**
+     * setter to eval properties.
+     * @since 1.6
+     */
+    public void setEvaluateProperties(boolean evaluateProperties) {
+        this.evaluateProperties=evaluateProperties;
+    }
+
+
+    /**
+     * read in a source file to a property
+     *
+     * @exception BuildException if something goes wrong with the build
+     */
+    public void execute()
+        throws BuildException {
+        //validation
+        if (srcFile == null) {
+            throw new BuildException("source file not defined");
+        }
+        if (property == null) {
+            throw new BuildException("output property not defined");
+        }
+        FileInputStream fis = null;
+        BufferedInputStream bis = null;
+        Reader instream = null;
+        log("loading "+srcFile+" into property "+property,Project.MSG_VERBOSE);
+        try {
+            long len = srcFile.length();
+            log("file size = "+len,Project.MSG_DEBUG);
+            //discard most of really big files
+            if (len > Integer.MAX_VALUE) {
+                log("this file is far to big to load completely");
+            }
+            int size=(int) len;
+            //open up the file
+            fis = new FileInputStream(srcFile);
+            bis = new BufferedInputStream(fis);
+            if (encoding == null) {
+                instream = new InputStreamReader(bis);
+            }
+            else {
+                instream = new InputStreamReader(bis, encoding);
+            }
+
+            String text = processStream(instream, size);
+
+            if(evaluateProperties) {
+                text = project.replaceProperties(text);
+            }
+            project.setNewProperty(property, text);
+            log("loaded " + text.length() + " characters",Project.MSG_VERBOSE);
+            log(property+" := "+text,Project.MSG_DEBUG);
+
+        } catch (IOException ioe) {
+            String message = "Unable to load file: " + ioe.toString();
+            if (failOnError) {
+                throw new BuildException(message, ioe, location);
+            }
+            else {
+                log(message, Project.MSG_ERR);
+            }
+        } finally {
+            try {
+                if (fis != null) {
+                    fis.close();
+                }
+            } catch (IOException ioex) {
+            }
+        }
+    }
+
+    /**
+     * Process the input by passing it through the reader chain.
+     */
+    private final String processStream(Reader instream, final int size)
+        throws IOException {
+
+        final char[] buffer = new char[size];
+        final int filterReadersCount = filterReaderSets.size();
+        final Vector finalFilters = new Vector();
+
+        for (int i = 0; i < filterReadersCount; i++) {
+            final FilterReaderSet filterset = (FilterReaderSet) filterReaderSets.elementAt(i);
+            final Vector filterReaders = filterset.getFilterReaders();
+            final int readerCount = filterReaders.size();
+            for (int j = 0; j < readerCount; j++) {
+                final AntFilterReader fr = (AntFilterReader) filterReaders.elementAt(j);
+                finalFilters.addElement(fr);
+            }
+        }
+
+        final int filtersCount = finalFilters.size();
+
+        if (filtersCount > 0) {
+            for (int i = 0; i < filtersCount; i++) {
+                final AntFilterReader filter = (AntFilterReader) finalFilters.elementAt(i);
+                final String clazz = filter.getClassName();
+                if (clazz != null) {
+                    try {
+                        final Class c = Class.forName(clazz);
+                        if (c != null) {
+                            final Constructor[] constructors = c.getConstructors();
+                            final Reader[] rdr = {instream};
+                            instream = (Reader) constructors[0].newInstance(rdr);
+                            if (Parameterizable.class.isAssignableFrom(c)) {
+                                final Hashtable params = filter.getParams();
+                                ((Parameterizable) instream).setParameters(params);
+                            }
+                        }
+                    } catch (final ClassNotFoundException cnfe) {
+                        throw new BuildException(cnfe);
+                    } catch (final InstantiationException ie) {
+                        throw new BuildException(ie);
+                    } catch (final IllegalAccessException iae) {
+                        throw new BuildException(iae);
+                    } catch (final InvocationTargetException ite) {
+                        throw new BuildException(ite);
+                    }
+                }
+            }
+        }
+
+        final int bufferLength = instream.read(buffer);
+        final String text = new String(buffer, 0, bufferLength);
+        return text;
+    }
+
+    /**
+     * Add the FilterReaderSet element.
+     */
+    public final void addFilterReaderSet(FilterReaderSet filter) {
+        filterReaderSets.addElement(filter);
+    }
+
+//end class
+}
