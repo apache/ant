3b1a100df553de6ae7f20b7d8d2e69c6a9646aba
Conor MacNeill <conor@apache.org>
2002-03-05 14:03:05 +0000

    Need to have these live in the right package
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@271746 13f79535-47bb-0310-9956-ffa450edef68

--- proposal/mutant/src/java/antlibs/ant1compat/org/apache/tools/ant/taskdefs/Ant.java
+++ proposal/mutant/src/java/antlibs/ant1compat/org/apache/tools/ant/taskdefs/Ant.java
@@ -51,7 +51,7 @@
  * information on the Apache Software Foundation, please see
  * <http://www.apache.org/>.
  */
-package org.apache.tools.ant;
+package org.apache.tools.ant.taskdefs;
 
 /**
  * Ant facade over system version of Ant
