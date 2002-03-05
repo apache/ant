b582e77a5e6af06bcfbe653764424226600bfece
No Author <dev-null@apache.org>
2002-03-05 07:54:00 +0000

    This commit was manufactured by cvs2svn to create tag 'MYRMIDON_PRE_CONF_MUNGE'.
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/tags/MYRMIDON_PRE_CONF_MUNGE@271738 13f79535-47bb-0310-9956-ffa450edef68

--- proposal/mutant/build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM   Copyright (c) 2000-2001 The Apache Software Foundation.  All rights
-REM   reserved.
-
-java -classpath bootstrap\lib\start.jar;bootstrap\lib\init.jar org.apache.ant.start.Main %*
