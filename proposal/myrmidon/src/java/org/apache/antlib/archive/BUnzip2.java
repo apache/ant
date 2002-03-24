ac202afe9153d226be2eac6ff79011dcb91f502d
Peter Donald <donaldp@apache.org>
2002-03-24 07:55:33 +0000

    Move to the external bzip package
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@272010 13f79535-47bb-0310-9956-ffa450edef68

--- proposal/myrmidon/src/java/org/apache/antlib/archive/BUnzip2.java
+++ proposal/myrmidon/src/java/org/apache/antlib/archive/BUnzip2.java
@@ -9,8 +9,8 @@ package org.apache.antlib.archive;
 
 import java.io.IOException;
 import java.io.InputStream;
-import org.apache.aut.bzip2.CBZip2InputStream;
 import org.apache.myrmidon.api.TaskException;
+import org.apache.avalon.excalibur.bzip2.CBZip2InputStream;
 
 /**
  * Expands a file that has been compressed with the BZIP2 algorithm. Normally
