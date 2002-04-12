17279312b558faecefb7527401824068e77fbda9
Stefan Bodewig <bodewig@apache.org>
2002-04-12 13:44:37 +0000

    2002
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@272406 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/taskdefs/optional/Native2Ascii.java
+++ src/main/org/apache/tools/ant/taskdefs/optional/Native2Ascii.java
@@ -1,7 +1,7 @@
 /*
  * The Apache Software License, Version 1.1
  *
- * Copyright (c) 2000 The Apache Software Foundation.  All rights 
+ * Copyright (c) 2000,2002 The Apache Software Foundation.  All rights 
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -71,6 +71,7 @@ import java.io.File;
  *
  * @author <a href="asudell@acm.org">Drew Sudell</a>
  * @author <a href="mailto:stefan.bodewig@epost.de">Stefan Bodewig</a>
+ * @since Ant 1.2
  */
 public class Native2Ascii extends MatchingTask {
 
@@ -258,7 +259,8 @@ public class Native2Ascii extends MatchingTask {
         public String[] mapFileName(String fileName) {
             int lastDot = fileName.lastIndexOf('.');
             if (lastDot >= 0) {
-                return new String[] {fileName.substring(0, lastDot) + extension};
+                return new String[] {fileName.substring(0, lastDot) 
+                                         + extension};
             } else {
                 return new String[] {fileName + extension};
             }
