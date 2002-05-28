aa8cb65d313f68d52bef0186b1c8b689bbb51cbb
Erik Hatcher <ehatcher@apache.org>
2002-05-28 00:33:36 +0000

    finally fix the XMLCatalog nightmare?!
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/branches/ANT_15_BRANCH@272787 13f79535-47bb-0310-9956-ffa450edef68

--- bootstrap.bat
+++ bootstrap.bat
@@ -26,7 +26,7 @@ if not "%OS%" == "Windows_NT" if exist bootstrap\nul deltree/y bootstrap
 if     "%OS%" == "Windows_NT" if exist build\nul rmdir/s/q build
 if not "%OS%" == "Windows_NT" if exist build\nul deltree/y build
 
-SET LOCALCLASSPATH=lib\xercesImpl.jar;lib\xmlParserAPIs.jar
+SET LOCALCLASSPATH=lib\xercesImpl.jar;lib\xml-apis.jar
 for %%i in (lib\optional\*.jar) do call src\script\lcp.bat %%i
 if exist "%JAVA_HOME%\lib\tools.jar" call src\script\lcp.bat %JAVA_HOME%\lib\tools.jar
 if exist "%JAVA_HOME%\lib\classes.zip" call src\script\lcp.bat %JAVA_HOME%\lib\classes.zip
