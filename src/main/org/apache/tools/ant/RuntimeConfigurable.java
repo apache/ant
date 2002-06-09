f49cac950b8b166ce5acf589bca177cc1bb3bc54
Conor MacNeill <conor@apache.org>
2002-06-09 10:04:07 +0000

    Allow RuntimeConfigurable to be reused Do not replace UnknownElements with their actual class so redefinitions get recreated.
    PR:	9675
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/branches/ANT_15_BRANCH@272854 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/RuntimeConfigurable.java
+++ src/main/org/apache/tools/ant/RuntimeConfigurable.java
@@ -61,8 +61,8 @@ import org.xml.sax.AttributeList;
 import org.xml.sax.helpers.AttributeListImpl;
 
 /**
- * Wrapper class that holds the attributes of an element, its children, and 
- * any text within it. It then takes care of configuring that element at 
+ * Wrapper class that holds the attributes of an element, its children, and
+ * any text within it. It then takes care of configuring that element at
  * runtime.
  *
  * @author <a href="mailto:stefan.bodewig@epost.de">Stefan Bodewig</a>
@@ -79,10 +79,12 @@ public class RuntimeConfigurable {
     private AttributeList attributes;
     /** Text appearing within the element. */
     private StringBuffer characters = new StringBuffer();
+    /** Indicates if the wrapped object has been configured */
+    private boolean proxyConfigured = false;
 
     /**
      * Sole constructor creating a wrapper for the specified object.
-     * 
+     *
      * @param proxy The element to configure. Must not be <code>null</code>.
      * @param elementTag The tag name generating this element.
      *                   Should not be <code>null</code>.
@@ -90,21 +92,23 @@ public class RuntimeConfigurable {
     public RuntimeConfigurable(Object proxy, String elementTag) {
         wrappedObject = proxy;
         this.elementTag = elementTag;
+        proxyConfigured = false;
     }
 
     /**
-     * Sets the element to configure. This is used when the real type of 
+     * Sets the element to configure. This is used when the real type of
      * an element isn't known at the time of wrapper creation.
-     * 
+     *
      * @param proxy The element to configure. Must not be <code>null</code>.
      */
     void setProxy(Object proxy) {
         wrappedObject = proxy;
+        proxyConfigured = false;
     }
 
     /**
      * Sets the attributes for the wrapped element.
-     * 
+     *
      * @param attributes List of attributes defined in the XML for this
      *                   element. May be <code>null</code>.
      */
@@ -114,7 +118,7 @@ public class RuntimeConfigurable {
 
     /**
      * Returns the list of attributes for the wrapped element.
-     * 
+     *
      * @return An AttributeList representing the attributes defined in the
      *         XML for this element. May be <code>null</code>.
      */
@@ -124,7 +128,7 @@ public class RuntimeConfigurable {
 
     /**
      * Adds a child element to the wrapped element.
-     * 
+     *
      * @param child The child element wrapper to add to this one.
      *              Must not be <code>null</code>.
      */
@@ -134,9 +138,9 @@ public class RuntimeConfigurable {
 
     /**
      * Returns the child wrapper at the specified position within the list.
-     * 
+     *
      * @param index The index of the child to return.
-     * 
+     *
      * @return The child wrapper at position <code>index</code> within the
      *         list.
      */
@@ -146,8 +150,8 @@ public class RuntimeConfigurable {
 
     /**
      * Adds characters from #PCDATA areas to the wrapped element.
-     * 
-     * @param data Text to add to the wrapped element. 
+     *
+     * @param data Text to add to the wrapped element.
      *        Should not be <code>null</code>.
      */
     public void addText(String data) {
@@ -156,12 +160,12 @@ public class RuntimeConfigurable {
 
     /**
      * Adds characters from #PCDATA areas to the wrapped element.
-     * 
+     *
      * @param buf A character array of the text within the element.
      *            Must not be <code>null</code>.
      * @param start The start element in the array.
      * @param count The number of characters to read from the array.
-     * 
+     *
      */
     public void addText(char[] buf, int start, int count) {
         addText(new String(buf, start, count));
@@ -169,7 +173,7 @@ public class RuntimeConfigurable {
 
     /**
      * Returns the tag name of the wrapped element.
-     * 
+     *
      * @return The tag name of the wrapped element. This is unlikely
      *         to be <code>null</code>, but may be.
      */
@@ -183,13 +187,13 @@ public class RuntimeConfigurable {
      * and then each child is configured and added. Each time the
      * wrapper is configured, the attributes and text for it are
      * reset.
-     * 
+     *
      * If the element has an <code>id</code> attribute, a reference
      * is added to the project as well.
-     * 
-     * @param p The project containing the wrapped element. 
+     *
+     * @param p The project containing the wrapped element.
      *          Must not be <code>null</code>.
-     * 
+     *
      * @exception BuildException if the configuration fails, for instance due
      *            to invalid attributes or children, or text being added to
      *            an element which doesn't accept it.
@@ -197,18 +201,20 @@ public class RuntimeConfigurable {
     public void maybeConfigure(Project p) throws BuildException {
         String id = null;
 
+        if (proxyConfigured) {
+            return;
+        }
+
         if (attributes != null) {
             ProjectHelper.configure(wrappedObject, attributes, p);
             id = attributes.getValue("id");
-            attributes = null;
         }
         if (characters.length() != 0) {
             ProjectHelper.addText(p, wrappedObject, characters.toString());
-            characters.setLength(0);
         }
         Enumeration enum = children.elements();
         while (enum.hasMoreElements()) {
-            RuntimeConfigurable child 
+            RuntimeConfigurable child
                 = (RuntimeConfigurable) enum.nextElement();
             if (child.wrappedObject instanceof Task) {
                 Task childTask = (Task) child.wrappedObject;
@@ -217,13 +223,14 @@ public class RuntimeConfigurable {
             } else {
                 child.maybeConfigure(p);
             }
-            ProjectHelper.storeChild(p, wrappedObject, child.wrappedObject, 
+            ProjectHelper.storeChild(p, wrappedObject, child.wrappedObject,
                 child.getElementTag().toLowerCase(Locale.US));
         }
 
         if (id != null) {
             p.addReference(id, wrappedObject);
         }
+        proxyConfigured = true;
     }
 
 }
