70936fadd1b6269662e3b8d24edf098ffafaa79f
Conor MacNeill <conor@apache.org>
2002-07-11 03:28:32 +0000

    Wow - look at all the things you can do in a single commit. No need to commit each directory separately. :-)
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@273087 13f79535-47bb-0310-9956-ffa450edef68

--- proposal/mutant/build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM   Copyright (c) 2002 The Apache Software Foundation.  All rights
-REM   reserved.
-
-java -jar bootstrap\lib\start.jar %*
--- proposal/mutant/lib/antlib/script/readme.txt
+++
@@ -1,1 +0,0 @@
-Please any jars in here which should be used when building the script antlib
