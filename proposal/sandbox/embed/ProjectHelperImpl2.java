a2e5b12e38496211e219a71d10252df3ea2fffb9
Costin Manolache <costin@apache.org>
2002-07-16 22:17:06 +0000

    First version of the <systemPath>.
    You need to include ant-sax2.jar in ant/lib, define a <path id='foo'> that includes
    all the jars that you need ( junit.jar, etc ) and then use
     <systemPath pathRef='foo' /> to create the loader and reload all optional tasks.
    
    After this step all optional tasks will work in the same way as if you have
    included the jars from the <path> definition in ant/lib.
    
    I did some basic tests and seems to work fine - note that <taskdef> will
    work as before, i.e. if you explicitely define tasks that need optional
    jars ( or redefine optional task ), you'll have the old behavior ( i.e.
    require things in CLASSPATH ).
    
    I'll try to find what changes are need for <import> and re-do the build
    for ant-sax2, then do more testings.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@273118 13f79535-47bb-0310-9956-ffa450edef68

--- proposal/sandbox/embed/ProjectHelperImpl2.java
+++ proposal/sandbox/embed/ProjectHelperImpl2.java
@@ -82,6 +82,7 @@ import javax.xml.parsers.SAXParserFactory;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.apache.tools.ant.types.SystemPath;
 /**
  * Sax2 based project reader
  *
@@ -97,39 +98,6 @@ public class ProjectHelperImpl2 extends ProjectHelper {
      */
     private static SAXParserFactory parserFactory = null;
 
-    /** Will prepare the class loader to allow dynamic modifications
-        of the classpath. Optional tasks are loaded in a different loader.
-    */
-    public void initClassLoader(Project project) {
-        try {
-            // reverse loader
-            AntClassLoader acl=new AntClassLoader( this.getClass().getClassLoader(), true );
-            acl.addLoaderPackageRoot( "org.apache.tools.ant.taskdefs.optional");
-
-            // XXX find the classpath
-            String antHome=project.getProperty( "ant.home" );
-            File optionalJar=new File( antHome + "/lib/optional.jar" );
-            System.out.println("Optional.jar = " +optionalJar.getAbsolutePath());
-            acl.addPathElement(optionalJar.getAbsolutePath() );
-            
-            // reload all optional tasks in this loader.
-            Hashtable tasks=project.getTaskDefinitions();
-            Enumeration keys=tasks.keys();
-            while( keys.hasMoreElements() ) {
-                String n=(String)keys.nextElement();
-                Class c=(Class)tasks.get(n);
-                if( ! c.getName().startsWith( "org.apache.tools.ant.taskdefs.optional" ))
-                    continue;
-                // System.out.println("Reloading " + n + " " + c + " " + c.getClassLoader() );
-                c=acl.loadClass( c.getName() );
-                tasks.put( n, c );
-                // System.out.println("Loaded " + n + " " + c.getClassLoader() );
-            }
-        } catch( Exception ex ) {
-            ex.printStackTrace();
-        }
-    }
-
     /**
      * Parses the project file, configuring the project as it goes.
      * 
@@ -137,9 +105,9 @@ public class ProjectHelperImpl2 extends ProjectHelper {
      *                           be read
      */
     public void parse(Project project, Object source) throws BuildException {
-        // re-init class loader for optional.jar
-        initClassLoader( project );
-
+        // Hook our one tasks.
+        project.addDataTypeDefinition( "systemPath" , SystemPath.class );
+        
         AntXmlContext context=new AntXmlContext();
         if(source instanceof File) {
             context.buildFile=(File)source;
