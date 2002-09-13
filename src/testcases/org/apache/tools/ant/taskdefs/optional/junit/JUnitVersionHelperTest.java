686cb8ffd17e68ce3595194f7b623d78b16f7251
Stefan Bodewig <bodewig@apache.org>
2002-09-13 10:03:11 +0000

    Provide a resonable name value for test implementations that are not TestCase subclasses - user their getName method if present.
    This should give better reports for tests that use jFunc or similar
    frameworks.
    
    PR: 11531
    Submitted by:	<ender at one.net>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@273322 13f79535-47bb-0310-9956-ffa450edef68

--- src/testcases/org/apache/tools/ant/taskdefs/optional/junit/JUnitVersionHelperTest.java
+++ src/testcases/org/apache/tools/ant/taskdefs/optional/junit/JUnitVersionHelperTest.java
@@ -1,7 +1,7 @@
 /*
  * The Apache Software License, Version 1.1
  *
- * Copyright (c) 2001-2002 The Apache Software Foundation.  All rights
+ * Copyright (c) 2002 The Apache Software Foundation.  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -51,59 +51,59 @@
  * information on the Apache Software Foundation, please see
  * <http://www.apache.org/>.
  */
-
 package org.apache.tools.ant.taskdefs.optional.junit;
 
-import java.lang.reflect.Method;
 import junit.framework.Test;
 import junit.framework.TestCase;
+import junit.framework.TestResult;
 
 /**
- * Work around for some changes to the public JUnit API between
- * different JUnit releases.
- *
- * @author <a href="mailto:stefan.bodewig@epost.de">Stefan Bodewig</a>
  * @version $Revision$
+ * @author <a href="mailto:stefan.bodewig@epost.de">Stefan Bodewig</a>
  */
-public class JUnitVersionHelper {
+public class JUnitVersionHelperTest extends TestCase {
 
-    private static Method testCaseName = null;
-    static {
-        try {
-            testCaseName = TestCase.class.getMethod("getName", new Class[0]);
-        } catch (NoSuchMethodException e) {
-            // pre JUnit 3.7
-            try {
-                testCaseName = TestCase.class.getMethod("name", new Class[0]);
-            } catch (NoSuchMethodException e2) {}
-        }
+    public JUnitVersionHelperTest(String name) {
+        super(name);
     }
 
-    /**
-     * JUnit 3.7 introduces TestCase.getName() and subsequent versions
-     * of JUnit remove the old name() method.  This method provides
-     * access to the name of a TestCase via reflection that is
-     * supposed to work with version before and after JUnit 3.7.
-     *
-     * <p>since Ant 1.5.1 this method will invoke &quot;<code>public
-     * String getName()</code>&quot; on any implementation of Test if
-     * it exists.</p>
-     */
-    public static String getTestCaseName(Test t) {
-        if (t instanceof TestCase && testCaseName != null) {
-            try {
-                return (String) testCaseName.invoke(t, new Object[0]);
-            } catch (Throwable e) {}
-        } else {
-            try {
-                Method getNameMethod = 
-                    t.getClass().getMethod("getName", new Class [0]);
-                if (getNameMethod.getReturnType() == String.class) {
-                    return (String) getNameMethod.invoke(t, new Object[0]);
-                }
-            } catch (Throwable e) {}
-        }
-        return "unknown";
+    public void testMyOwnName() {
+        assertEquals("testMyOwnName", 
+                     JUnitVersionHelper.getTestCaseName(this));
     }
 
+    public void testNonTestCaseName() {
+        assertEquals("I'm a foo", 
+                     JUnitVersionHelper.getTestCaseName(new Foo1()));
+    }
+
+    public void testNoStringReturn() {
+        assertEquals("unknown", 
+                     JUnitVersionHelper.getTestCaseName(new Foo2()));
+    }
+
+    public void testNoGetName() {
+        assertEquals("unknown", 
+                     JUnitVersionHelper.getTestCaseName(new Foo3()));
+    }
+
+    public void testNull() {
+        assertEquals("unknown", JUnitVersionHelper.getTestCaseName(null));
+    }
+
+    public static class Foo implements Test {
+        public int countTestCases() {return 0;}
+        public void run(TestResult result) {}
+    }
+
+    public static class Foo1 extends Foo {
+        public String getName() {return "I'm a foo";}
+    }
+
+    public static class Foo2 extends Foo {
+        public int getName() {return 1;}
+    }
+
+    public static class Foo3 extends Foo {
+    }
 }
