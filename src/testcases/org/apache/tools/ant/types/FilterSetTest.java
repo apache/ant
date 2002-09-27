14e9802e433027f14367526f33141a6b9433102e
Stefan Bodewig <bodewig@apache.org>
2002-09-27 09:38:23 +0000

    merge nested filtersets fix from CVS HEAD
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/branches/ANT_15_BRANCH@273382 13f79535-47bb-0310-9956-ffa450edef68

--- src/testcases/org/apache/tools/ant/types/FilterSetTest.java
+++ src/testcases/org/apache/tools/ant/types/FilterSetTest.java
@@ -62,6 +62,7 @@ import junit.framework.TestCase;
 import junit.framework.AssertionFailedError;
 
 import java.io.*;
+import java.util.Hashtable;
 
 /**
  * FilterSet testing
@@ -100,6 +101,31 @@ public class FilterSetTest extends BuildFileTest {
         executeTarget("test3");
         assertTrue("Filterset 3 failed", compareFiles("src/etc/testcases/types/gold/filterset3.txt",
                                                       "src/etc/testcases/types/dest3.txt"));
+    }
+
+    public void testNestedFilterSets() {
+        executeTarget("test-nested-filtersets");
+
+        FilterSet fs = (FilterSet) getProject().getReference("1");
+        Hashtable filters = fs.getFilterHash();
+        assertEquals(1, filters.size());
+        assertEquals("value1", filters.get("token1"));
+
+        fs = (FilterSet) getProject().getReference("2");
+        filters = fs.getFilterHash();
+        assertEquals(2, filters.size());
+        assertEquals("1111", filters.get("aaaa"));
+        assertEquals("2222", filters.get("bbbb"));
+
+        fs = (FilterSet) getProject().getReference("3");
+        filters = fs.getFilterHash();
+        assertEquals(1, filters.size());
+        assertEquals("value4", filters.get("token4"));
+
+        fs = (FilterSet) getProject().getReference("5");
+        filters = fs.getFilterHash();
+        assertEquals(1, filters.size());
+        assertEquals("value1", filters.get("token1"));
     }
 
     private boolean compareFiles(String name1, String name2) {
