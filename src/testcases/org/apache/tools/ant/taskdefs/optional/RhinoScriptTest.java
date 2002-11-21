ad49e2a62d2e8751596d273ea2f639648c7d1b45
Stefan Bodewig <bodewig@apache.org>
2002-11-21 08:49:37 +0000

    Add tests for <script>
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@273564 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/testcases/org/apache/tools/ant/taskdefs/optional/RhinoScriptTest.java
@@ -0,0 +1,106 @@
+/*
+ *  The Apache Software License, Version 1.1
+ *
+ *  Copyright (c) 2002 The Apache Software Foundation.  All rights
+ *  reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright
+ *  notice, this list of conditions and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *  notice, this list of conditions and the following disclaimer in
+ *  the documentation and/or other materials provided with the
+ *  distribution.
+ *
+ *  3. The end-user documentation included with the redistribution, if
+ *  any, must include the following acknowlegement:
+ *  "This product includes software developed by the
+ *  Apache Software Foundation (http://www.apache.org/)."
+ *  Alternately, this acknowlegement may appear in the software itself,
+ *  if and wherever such third-party acknowlegements normally appear.
+ *
+ *  4. The names "The Jakarta Project", "Ant", and "Apache Software
+ *  Foundation" must not be used to endorse or promote products derived
+ *  from this software without prior written permission. For written
+ *  permission, please contact apache@apache.org.
+ *
+ *  5. Products derived from this software may not be called "Apache"
+ *  nor may "Apache" appear in their names without prior written
+ *  permission of the Apache Group.
+ *
+ *  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ *  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ *  ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ *  USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ *  OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ *  SUCH DAMAGE.
+ *  ====================================================================
+ *
+ *  This software consists of voluntary contributions made by many
+ *  individuals on behalf of the Apache Software Foundation.  For more
+ *  information on the Apache Software Foundation, please see
+ *  <http://www.apache.org/>.
+ */
+package org.apache.tools.ant.taskdefs.optional;
+
+import org.apache.tools.ant.BuildFileTest;
+
+/**
+ * Tests the examples of the &lt;script&gt; task docs.
+ *
+ * @author <a href="mailto:stefan.bodewig@epost.de">Stefan Bodewig</a>
+ * @since Ant 1.5.2
+ */
+public class RhinoScriptTest extends BuildFileTest {
+
+    public RhinoScriptTest(String name) {
+        super(name);
+    }
+
+    /**
+     * The JUnit setup method
+     */
+    public void setUp() {
+        configureProject("src/etc/testcases/taskdefs/optional/script.xml");
+    }
+
+    public void testExample1() {
+        executeTarget("example1");
+        int index = getLog().indexOf("1");
+        assertTrue(index > -1);
+        index = getLog().indexOf("4", index);
+        assertTrue(index > -1);
+        index = getLog().indexOf("9", index);
+        assertTrue(index > -1);
+        index = getLog().indexOf("16", index);
+        assertTrue(index > -1);
+        index = getLog().indexOf("25", index);
+        assertTrue(index > -1);
+        index = getLog().indexOf("36", index);
+        assertTrue(index > -1);
+        index = getLog().indexOf("49", index);
+        assertTrue(index > -1);
+        index = getLog().indexOf("64", index);
+        assertTrue(index > -1);
+        index = getLog().indexOf("81", index);
+        assertTrue(index > -1);
+        index = getLog().indexOf("100", index);
+        assertTrue(index > -1);
+    }
+
+    public void testExample2() {
+        executeTarget("example2");
+        assertTrue(getLog().indexOf("In sub1") > -1);
+        assertTrue(getLog().indexOf("In sub2") > -1);
+    }
+}
