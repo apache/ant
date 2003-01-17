56bd149950453d839067fa8c812eb099bc8de226
Stefan Bodewig <bodewig@apache.org>
2003-01-17 14:46:51 +0000

    Merge fix for bug 15399 from HEAD
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/branches/ANT_15_BRANCH@273825 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/util/FileUtils.java
+++ src/main/org/apache/tools/ant/util/FileUtils.java
@@ -1,7 +1,7 @@
 /*
  * The Apache Software License, Version 1.1
  *
- * Copyright (c) 2001-2002 The Apache Software Foundation.  All rights
+ * Copyright (c) 2001-2003 The Apache Software Foundation.  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -682,7 +682,7 @@ public class FileUtils {
         synchronized (rand) {
             do {
                 result = new File(parent,
-                                  prefix + fmt.format(rand.nextInt())
+                                  prefix + fmt.format(Math.abs(rand.nextInt()))
                                   + suffix);
             } while (result.exists());
         }
