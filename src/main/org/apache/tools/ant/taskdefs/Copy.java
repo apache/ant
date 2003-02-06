3fa7a29dabfc85cf70ef0b41b66550048d10e653
Stefan Bodewig <bodewig@apache.org>
2003-02-06 16:21:10 +0000

    Merge patch for bug 16768 from HEAD
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/branches/ANT_15_BRANCH@274009 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/taskdefs/Copy.java
+++ src/main/org/apache/tools/ant/taskdefs/Copy.java
@@ -1,7 +1,7 @@
 /*
  * The Apache Software License, Version 1.1
  *
- * Copyright (c) 2000-2002 The Apache Software Foundation.  All rights
+ * Copyright (c) 2000-2003 The Apache Software Foundation.  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -532,6 +532,10 @@ public class Copy extends Task {
                 } catch (IOException ioe) {
                     String msg = "Failed to copy " + fromFile + " to " + toFile
                         + " due to " + ioe.getMessage();
+                    File targetFile = new File(toFile);
+                    if (targetFile.exists() && !targetFile.delete()) {
+                        msg += " and I couldn't delete the corrupt " + toFile;
+                    }
                     throw new BuildException(msg, ioe, getLocation());
                 }
             }
