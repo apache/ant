b1de9ab9c18532d6526fbd8d77e54ed78eafaa43
Stefan Bodewig <bodewig@apache.org>
2003-04-09 14:02:23 +0000

    Demonstrate bug 18670
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@274419 13f79535-47bb-0310-9956-ffa450edef68

--- src/testcases/org/apache/tools/ant/taskdefs/ChecksumTest.java
+++ src/testcases/org/apache/tools/ant/taskdefs/ChecksumTest.java
@@ -1,7 +1,7 @@
 /*
  * The Apache Software License, Version 1.1
  *
- * Copyright (c) 2001 The Apache Software Foundation.  All rights
+ * Copyright (c) 2001,2003 The Apache Software Foundation.  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -100,6 +100,11 @@ public class ChecksumTest extends BuildFileTest {
     public void testVerifyAsCondition() {
         testVerify("verifyAsCondition");
         assertNull(project.getProperty("no.logo.md5"));
+    }
+
+    public void testVerifyFromProperty() {
+        assertNull(getProject().getProperty("verify"));
+        expectPropertySet("verifyFromProperty", "verify", "true");
     }
 
     private void testVerify(String target) {
