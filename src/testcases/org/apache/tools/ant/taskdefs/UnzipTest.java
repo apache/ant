fddadc930e92238d00cad244733d3f6f2e5b9941
Stefan Bodewig <bodewig@apache.org>
2003-06-23 14:47:02 +0000

    Demonstrate Bugzilla Report 20969
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@274691 13f79535-47bb-0310-9956-ffa450edef68

--- src/testcases/org/apache/tools/ant/taskdefs/UnzipTest.java
+++ src/testcases/org/apache/tools/ant/taskdefs/UnzipTest.java
@@ -143,4 +143,16 @@ public class UnzipTest extends BuildFileTest {
                             + "probably a self-extracting archive");
     }
 
+
+    /*
+     * PR 20969
+     */
+    public void testPatternSetSlashOnly() {
+        executeTarget("testPatternSetSlashOnly");
+        assertTrue("1/foo is not included",
+                   !getProject().resolveFile("unziptestout/1/foo").exists());
+        assertTrue("2/bar is included",
+                   getProject().resolveFile("unziptestout/2/bar").exists());
+    }
+
 }
