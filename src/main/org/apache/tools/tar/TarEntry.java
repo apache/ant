153a8fa06cb10c40247ae0aa443583221e6dd04a
Stefan Bodewig <bodewig@apache.org>
2003-07-24 13:14:31 +0000

    Fix incorrect equals() override
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@274924 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/tar/TarEntry.java
+++ src/main/org/apache/tools/tar/TarEntry.java
@@ -326,6 +326,20 @@ public class TarEntry implements TarConstants {
     }
 
     /**
+     * Determine if the two entries are equal. Equality is determined
+     * by the header names being equal.
+     *
+     * @param it Entry to be checked for equality.
+     * @return True if the entries are equal.
+     */
+    public boolean equals(Object it) {
+        if (it == null || getClass() != it.getClass()) {
+            return false;
+        }
+        return equals((TarEntry) it);
+    }
+
+    /**
      * Hashcodes are based on entry names.
      *
      * @return the entry hashcode
