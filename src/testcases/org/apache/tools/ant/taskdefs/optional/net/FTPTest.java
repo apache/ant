1c3832c2f36797b7440228a66e5dea09e0f9803e
Antoine Levy-Lambert <antoine@apache.org>
2003-08-05 23:18:25 +0000

    Added a testcase showing resolution of PR 14063 Ant ftp did not download file it is a symlink PR: 14063
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@275030 13f79535-47bb-0310-9956-ffa450edef68

--- src/testcases/org/apache/tools/ant/taskdefs/optional/net/FTPTest.java
+++ src/testcases/org/apache/tools/ant/taskdefs/optional/net/FTPTest.java
@@ -241,7 +241,24 @@ public class FTPTest extends BuildFileTest{
         ds.scan();
         compareFiles(ds, new String[] {}, new String[] {});
     }
-
+    public void testFileSymlink() {
+        if (!supportsSymlinks) {
+            return;
+        }
+        if (!loginSuceeded) {
+            return;
+        }
+        if (!changeRemoteDir(remoteTmpDir)) {
+            return;
+        }
+        getProject().executeTarget("symlink-file-setup");
+        FTP.FTPDirectoryScanner ds = myFTPTask.newScanner(ftp);
+        ds.setBasedir(new File(getProject().getBaseDir(), "tmp"));
+        ds.setIncludes(new String[] {"alpha/beta/gamma/"});
+        ds.scan();
+        compareFiles(ds, new String[] {"alpha/beta/gamma/gamma.xml"},
+                     new String[] {"alpha/beta/gamma"});
+    }
     // father and child pattern test
     public void testOrderOfIncludePatternsIrrelevant() {
         if (!loginSuceeded) {
