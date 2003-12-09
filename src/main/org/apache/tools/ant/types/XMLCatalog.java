2fb88278346610d5495bcbac7bd98d79563c37f2
Antoine Levy-Lambert <antoine@apache.org>
2003-12-09 01:17:31 +0000

    Remove unused import added by mistake
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@275760 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/types/XMLCatalog.java
+++ src/main/org/apache/tools/ant/types/XMLCatalog.java
@@ -74,7 +74,6 @@ import javax.xml.transform.sax.SAXSource;
 import org.apache.tools.ant.AntClassLoader;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Project;
-import org.apache.tools.ant.taskdefs.condition.Os;
 import org.apache.tools.ant.util.FileUtils;
 import org.apache.tools.ant.util.JAXPUtils;
 import org.xml.sax.EntityResolver;
