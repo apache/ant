2c655cd47b0abab3a3e8680fc61f5b983889be04
Peter Reilly <peterreilly@apache.org>
2004-01-07 10:11:38 +0000

    Sync with head
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/branches/ANT_16_BRANCH@275862 13f79535-47bb-0310-9956-ffa450edef68

--- src/testcases/org/apache/tools/ant/taskdefs/CopyTest.java
+++ src/testcases/org/apache/tools/ant/taskdefs/CopyTest.java
@@ -1,7 +1,7 @@
 /*
  * The Apache Software License, Version 1.1
  *
- * Copyright (c) 2000-2003 The Apache Software Foundation.  All rights
+ * Copyright (c) 2000-2004 The Apache Software Foundation.  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -126,6 +126,11 @@ public class CopyTest extends BuildFileTest {
 
     }
 
+    public void testFilterTest() {
+        executeTarget("filtertest");
+        assertTrue(getOutput().indexOf("loop in tokens") == -1);
+    }
+    
     public void testFilterSet() throws IOException {
         executeTarget("testFilterSet");
         FileUtils fileUtils = FileUtils.newFileUtils();
