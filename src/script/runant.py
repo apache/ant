96f9d3789908c1440fa38d6193e3705ef96be6e1
Antoine Levy-Lambert <antoine@apache.org>
2004-02-06 00:15:17 +0000

    Merge with HEAD Copyright Notice
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/branches/ANT_16_BRANCH@276032 13f79535-47bb-0310-9956-ffa450edef68

--- src/script/runant.py
+++ src/script/runant.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python
+# Copyright 2001,2003-2004 Apache Software Foundation
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
 """
 
  runant.py
@@ -7,9 +22,6 @@
     It runs ant with/out arguments, it should be quite portable (thanks to
     the python os library)
     This script has been tested with Python2.0/Win2K
-
-        Copyright (c) 2001,2003 The Apache Software Foundation.  All rights
-        reserved.
 
  created:         2001-04-11
  author:          Pierre Dittgen pierre.dittgen@criltelecom.com
