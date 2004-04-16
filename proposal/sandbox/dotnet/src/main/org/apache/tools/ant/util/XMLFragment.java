838247a53b1e983f9b33c6e2fb06eea9a9cf1511
Stefan Bodewig <bodewig@apache.org>
2004-04-16 09:15:32 +0000

    An Ant 1.6.x ready version, basically 1.7's XMLFragment has been merged into the antlib
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/branches/ANT_16_BRANCH@276326 13f79535-47bb-0310-9956-ffa450edef68

---
+++ proposal/sandbox/dotnet/src/main/org/apache/tools/ant/util/XMLFragment.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright  2003-2004 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package org.apache.tools.ant.util;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.DocumentFragment;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.Text;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.DynamicConfigurator;
+import org.apache.tools.ant.ProjectHelper;
+
+/**
+ * Use this class as a nested element if you want to get a literal DOM
+ * fragment of something nested into your task/type.
+ *
+ * <p>This is useful for tasks that want to deal with the "real" XML
+ * from the build file instead of objects.</p>
+ *
+ * <p>Code heavily influenced by code written by Dominique Devienne.</p>
+ *
+ * @since Ant 1.7
+ */
+public class XMLFragment implements DynamicConfigurator {
+
+    private Document doc;
+    private DocumentFragment fragment;
+
+    public XMLFragment() {
+        doc = JAXPUtils.getDocumentBuilder().newDocument();
+        fragment = doc.createDocumentFragment();
+    }
+
+    /**
+     * Return the DocumentFragment that corresponds to the nested
+     * structure.
+     */
+    public DocumentFragment getFragment() {
+        return fragment;
+    }
+
+    /**
+     * Add nested text.
+     */
+    public void addText(String s) {
+        addText(fragment, s);
+    }
+
+    /**
+     * No attributes for the wrapping element.
+     */
+    public void setDynamicAttribute(String name, String value)
+        throws BuildException {
+        throw new BuildException("Attribute " + name + " is not supported.");
+    }
+
+    /**
+     * Creates a nested element.
+     */
+    public Object createDynamicElement(String name) {
+        Element e = doc
+            .createElementNS(ProjectHelper.extractUriFromComponentName(name),
+                             ProjectHelper.extractNameFromComponentName(name));
+        fragment.appendChild(e);
+        return new Child(e);
+    }
+
+    private void addText(Node n, String s) {
+        if (s != null && !s.trim().equals("")) {
+            Text t = doc.createTextNode(s);
+            n.appendChild(t);
+        }
+    }
+
+    public class Child implements DynamicConfigurator {
+        private Element e;
+
+        Child(Element e) {
+            this.e = e;
+        }
+
+        /**
+         * Add nested text.
+         */
+        public void addText(String s) {
+            XMLFragment.this.addText(e, s);
+        }
+
+        /**
+         * Sets the attribute
+         */
+        public void setDynamicAttribute(String name, String value) {
+            e.setAttribute(name, value);
+        }
+
+        /**
+         * Creates a nested element.
+         */
+        public Object createDynamicElement(String name) {
+            Element e2 = doc
+                .createElementNS(ProjectHelper
+                                 .extractUriFromComponentName(name),
+                                 ProjectHelper
+                                 .extractNameFromComponentName(name));
+            e.appendChild(e2);
+            return new Child(e2);
+        }
+    }
+
+}
