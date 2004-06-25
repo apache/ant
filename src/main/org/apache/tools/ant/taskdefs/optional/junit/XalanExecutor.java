625f6460d93ed4dd26c5a9399b69ff9a62da030a
Stefan Bodewig <bodewig@apache.org>
2004-06-25 13:50:16 +0000

    typos
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/branches/ANT_16_BRANCH@276644 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/taskdefs/optional/junit/XalanExecutor.java
+++ src/main/org/apache/tools/ant/taskdefs/optional/junit/XalanExecutor.java
@@ -33,9 +33,12 @@ import org.apache.tools.ant.util.JavaEnvUtils;
  * Command class that encapsulate specific behavior for each
  * Xalan version. The right executor will be instantiated at
  * runtime via class lookup. For instance, it will check first
- * for Xalan2, then for Xalan1.
+ * for Xalan2/XSLTC, then for Xalan1.
  */
 abstract class XalanExecutor {
+    private static final String pack = 
+        "org.apache.tools.ant.taskdefs.optional.junit.";
+
     /** the transformer caller */
     protected AggregateTransformer caller;
 
@@ -45,7 +48,7 @@ abstract class XalanExecutor {
     }
 
     /** get the appropriate stream based on the format (frames/noframes) */
-    protected OutputStream getOutputStream() throws IOException {
+    protected final OutputStream getOutputStream() throws IOException {
         if (AggregateTransformer.FRAMES.equals(caller.format)) {
             // dummy output for the framed report
             // it's all done by extension...
@@ -66,53 +69,73 @@ abstract class XalanExecutor {
      * @throws BuildException thrown if it could not find a valid xalan
      * executor.
      */
-    static XalanExecutor newInstance(AggregateTransformer caller) throws BuildException {
-        Class procVersion = null;
+    static XalanExecutor newInstance(AggregateTransformer caller) 
+        throws BuildException {
         XalanExecutor executor = null;
         try {
-            procVersion = Class.forName("org.apache.xalan.processor.XSLProcessorVersion");
-            executor = (XalanExecutor) Class.forName(
-                "org.apache.tools.ant.taskdefs.optional.junit.Xalan2Executor").newInstance();
-        } catch (Exception xalan2missing) {
-            StringWriter swr = new StringWriter();
-            xalan2missing.printStackTrace(new PrintWriter(swr));
-            caller.task.log("Didn't find Xalan2.", Project.MSG_DEBUG);
-            caller.task.log(swr.toString(), Project.MSG_DEBUG);
+            Class clazz = Class.forName(pack + "Xalan2Executor");
+            executor = (XalanExecutor)clazz.newInstance();
+        } catch (Exception xsltcApacheMissing){
+            caller.task.log(xsltcApacheMissing.toString());
             try {
-                procVersion = Class.forName("org.apache.xalan.xslt.XSLProcessorVersion");
-                executor = (XalanExecutor) Class.forName(
-                    "org.apache.tools.ant.taskdefs.optional.junit.Xalan1Executor").newInstance();
-            } catch (Exception xalan1missing) {
-                swr = new StringWriter();
-                xalan1missing.printStackTrace(new PrintWriter(swr));
-                caller.task.log("Didn't find Xalan1.", Project.MSG_DEBUG);
-                caller.task.log(swr.toString(), Project.MSG_DEBUG);
-                String msg = "Could not find xalan2 nor xalan1 "
-                    + "in the classpath. Check http://xml.apache.org/xalan-j/";
-                if (!JavaEnvUtils.isJavaVersion(JavaEnvUtils.JAVA_1_1)
-                    && !JavaEnvUtils.isJavaVersion(JavaEnvUtils.JAVA_1_2)
-                    && !JavaEnvUtils.isJavaVersion(JavaEnvUtils.JAVA_1_3)
-                    && !JavaEnvUtils.isJavaVersion(JavaEnvUtils.JAVA_1_4)) {
-                    msg += "\r\nStarting with JDK 1.5, the built-in processor "
-                        + "of the JDK is no longer Xalan\r\nbut XSLTC which is "
-                        + "not (yet) supported by the junitreport task.";
-                }
-                throw new BuildException(msg);
+                Class clazz = Class.forName(pack + "Xalan1Executor");
+                executor = (XalanExecutor) clazz.newInstance();
+            } catch (Exception xalan1Missing){
+                caller.task.log(xalan1Missing.toString());
+                throw new BuildException("Could not find xstlc nor xalan2 nor "
+                                         + "xalan1 in the classpath. Check "
+                                         + "http://xml.apache.org/xalan-j");
             }
         }
-        String version = getXalanVersion(procVersion);
-        caller.task.log("Using Xalan version: " + version);
+        String classNameImpl = executor.getImplementation();
+        String version = executor.getProcVersion(classNameImpl);
+        caller.task.log("Using " + version, Project.MSG_VERBOSE);
         executor.setCaller(caller);
         return executor;
     }
 
+    /**
+     * This methods should return the classname implementation of the
+     * underlying xslt processor
+     * @return the classname of the implementation, for example:
+     * org.apache.xalan.processor.TransformerFactoryImpl
+     * @see #getProcVersion(String)
+     */
+    protected abstract String getImplementation();
+
+    /**
+     * Try to discover the xslt processor version based on the
+     * className. There is nothing carved in stone and it can change
+     * anytime, so this is just for the sake of giving additional
+     * information if we can find it.
+     * @param classNameImpl the classname of the underlying xslt processor
+     * @return a string representing the implementation version.
+     * @throws BuildException
+     */
+    protected abstract String getProcVersion(String classNameImpl) 
+        throws BuildException;
+
+    /** a bit simplistic but xsltc data are conveniently private non final */
+    protected final String getXSLTCVersion(String procVersionClassName) 
+        throws ClassNotFoundException {
+        // there's a convenient xsltc class version but data are
+        // private so use package information
+        Class procVersion = Class.forName(procVersionClassName);
+        Package pkg = procVersion.getPackage();
+        return pkg.getName() + " " + pkg.getImplementationTitle() 
+            + " " + pkg.getImplementationVersion();
+    }
+
     /** pretty useful data (Xalan version information) to display. */
-    private static String getXalanVersion(Class procVersion) {
+    protected final String getXalanVersion(String procVersionClassName) 
+        throws ClassNotFoundException {
+        Class procVersion = Class.forName(procVersionClassName);
+        String pkg = procVersion.getPackage().getName();
         try {
             Field f = procVersion.getField("S_VERSION");
-            return f.get(null).toString();
+            return pkg + " " + f.get(null).toString();
         } catch (Exception e) {
-            return "?";
+            return pkg + " ?.?";
         }
     }
 }
