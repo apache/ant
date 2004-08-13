5d26c58ca06291641603a5ad97c64687cfa7ca96
Antoine Levy-Lambert <antoine@apache.org>
2004-08-13 09:27:01 +0000

    Add @since tags to methods introduced in Ant 1.6.2 PR: 30276 Submitted by: Jesse Glick
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@276779 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/Target.java
+++ src/main/org/apache/tools/ant/Target.java
@@ -80,6 +80,7 @@ public class Target implements TaskContainer {
      * Sets the location of this target's definition.
      *
      * @param location   <CODE>Location</CODE>
+     * @since 1.6.2
      */
     public void setLocation(Location location) {
         this.location = location;
@@ -89,6 +90,7 @@ public class Target implements TaskContainer {
      * Get the location of this target's definition.
      *
      * @return <CODE>Location</CODE>
+     * @since 1.6.2
      */
     public Location getLocation() {
         return location;
@@ -247,6 +249,7 @@ public class Target implements TaskContainer {
      *
      * @return the "if" property condition or <code>null</code> if no
      *         "if" condition had been defined.
+     * @since 1.6.2
      */
     public String getIf() {
         return ("".equals(ifCondition) ? null : ifCondition);
@@ -274,6 +277,7 @@ public class Target implements TaskContainer {
      *
      * @return the "unless" property condition or <code>null</code>
      *         if no "unless" condition had been defined.
+     * @since 1.6.2
      */
     public String getUnless() {
         return ("".equals(unlessCondition) ? null : unlessCondition);
