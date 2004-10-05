f6c27e5849c51b841f64a946447a3c171b18d9fc
Peter Reilly <peterreilly@apache.org>
2004-10-05 08:09:31 +0000

    typo
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@276911 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/Main.java
+++ src/main/org/apache/tools/ant/Main.java
@@ -838,7 +838,7 @@ public class Main implements AntMain {
         msg.append("  -nice  number          A niceness value for the main thread:" + lSep
                    + "                         1 (lowest) to 10 (highest); 5 is the default"
                    + lSep);
-        msg.append("  -nouserlib             Run ant within using the jar files from ${user.home}/"
+        msg.append("  -nouserlib             Run ant without using the jar files from ${user.home}/"
                    + ".ant/lib");
         System.out.println(msg.toString());
     }
