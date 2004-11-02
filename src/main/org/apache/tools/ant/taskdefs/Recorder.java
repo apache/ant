3012c097f13a5817ec571e748d1203d5cbfebd49
Stefan Bodewig <bodewig@apache.org>
2004-11-02 16:22:28 +0000

    merge
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/branches/ANT_16_BRANCH@276988 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/taskdefs/Recorder.java
+++ src/main/org/apache/tools/ant/taskdefs/Recorder.java
@@ -146,7 +146,15 @@ public class Recorder extends Task {
         RecorderEntry recorder = getRecorder(filename, getProject());
         // set the values on the recorder
         recorder.setMessageOutputLevel(loglevel);
-        recorder.setRecordState(start);
+        if (start != null) {
+            if (start.booleanValue()) {
+                recorder.reopenFile();
+                recorder.setRecordState(start);
+            } else {
+                recorder.setRecordState(start);
+                recorder.closeFile();
+            }
+        }
         recorder.setEmacsMode(emacsMode);
     }
 
@@ -193,23 +201,12 @@ public class Recorder extends Task {
 
         if (o == null) {
             // create a recorder entry
-            try {
-                entry = new RecorderEntry(name);
+            entry = new RecorderEntry(name);
 
-                PrintStream out = null;
-
-                if (append == null) {
-                    out = new PrintStream(
-                        new FileOutputStream(name));
-                } else {
-                    out = new PrintStream(
-                        new FileOutputStream(name, append.booleanValue()));
-                }
-                entry.setErrorPrintStream(out);
-                entry.setOutputPrintStream(out);
-            } catch (IOException ioe) {
-                throw new BuildException("Problems creating a recorder entry",
-                    ioe);
+            if (append == null) {
+                entry.openFile(false);
+            } else {
+                entry.openFile(append.booleanValue());
             }
             entry.setProject(proj);
             recorderEntries.put(name, entry);
