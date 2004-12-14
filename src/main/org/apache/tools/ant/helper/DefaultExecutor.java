c1e8da505228780d7eb73d69eb358811872e263d
No Author <dev-null@apache.org>
2004-12-14 23:48:01 +0000

    This commit was manufactured by cvs2svn to create branch 'ANT_16_BRANCH'.
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/branches/ANT_16_BRANCH@277201 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/org/apache/tools/ant/helper/DefaultExecutor.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2004 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package org.apache.tools.ant.helper;
+
+
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.Executor;
+import org.apache.tools.ant.BuildException;
+
+
+/**
+ * Default Target executor implementation. Runs each target individually
+ * (including all of its dependencies), halting immediately upon error.
+ * @since Ant 1.6.3
+ */
+public class DefaultExecutor implements Executor {
+
+    //inherit doc
+    public void executeTargets(Project project, String[] targetNames)
+        throws BuildException {
+        for (int i = 0; i < targetNames.length; i++) {
+            project.executeTarget(targetNames[i]);
+        }
+    }
+
+}
