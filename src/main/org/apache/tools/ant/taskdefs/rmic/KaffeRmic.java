c46ab46a5180a58d82749a799464dcb7ddff3010
Stefan Bodewig <bodewig@apache.org>
2004-12-15 12:31:44 +0000

    merge
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/branches/ANT_16_BRANCH@277209 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/taskdefs/rmic/KaffeRmic.java
+++ src/main/org/apache/tools/ant/taskdefs/rmic/KaffeRmic.java
@@ -29,33 +29,74 @@ import org.apache.tools.ant.types.Commandline;
  * @since Ant 1.4
  */
 public class KaffeRmic extends DefaultRmicAdapter {
+    public static final String RMIC_CLASSNAME = "gnu.java.rmi.rmic.RMIC";
+    // pre Kaffe 1.1.2
+    private static final String OLD_RMIC_CLASSNAME = "kaffe.rmi.rmic.RMIC";
+
+    /**
+     * the name of this adapter for users to select
+     */
+    public static final String COMPILER_NAME = "kaffe";
+
 
     public boolean execute() throws BuildException {
         getRmic().log("Using Kaffe rmic", Project.MSG_VERBOSE);
         Commandline cmd = setupRmicCommand();
 
-        try {
-
-            Class c = Class.forName("kaffe.rmi.rmic.RMIC");
-            Constructor cons = c.getConstructor(new Class[] {String[].class});
-            Object rmic = cons.newInstance(new Object[] {cmd.getArguments()});
-            Method doRmic = c.getMethod("run", null);
-            Boolean ok = (Boolean) doRmic.invoke(rmic, null);
-
-            return ok.booleanValue();
-        } catch (ClassNotFoundException ex) {
+        Class c = getRmicClass();
+        if (c == null) {
             throw new BuildException("Cannot use Kaffe rmic, as it is not "
-                                     + "available.  A common solution is to "
+                                     + "available.  Neither "
+                                     + RMIC_CLASSNAME
+                                     + " nor "
+                                     + OLD_RMIC_CLASSNAME
+                                     + " have been found.  "
+                                     + "A common solution is to "
                                      + "set the environment variable "
                                      + "JAVA_HOME or CLASSPATH.",
                                      getRmic().getLocation());
+        }
+
+        try {
+            Constructor cons = c.getConstructor(new Class[] {String[].class});
+            Object rmic = cons.newInstance(new Object[] {cmd.getArguments()});
+            Method doRmic = c.getMethod("run", (Class[]) null);
+            Boolean ok = (Boolean) doRmic.invoke(rmic, (Object[]) null);
+
+            return ok.booleanValue();
+        } catch (BuildException ex) {
+            //rethrow
+            throw ex;
         } catch (Exception ex) {
-            if (ex instanceof BuildException) {
-                throw (BuildException) ex;
-            } else {
-                throw new BuildException("Error starting Kaffe rmic: ",
-                                         ex, getRmic().getLocation());
+            //wrap
+           throw new BuildException("Error starting Kaffe rmic: ",
+                                    ex, getRmic().getLocation());
+        }
+    }
+
+    /**
+     * test for kaffe being on the system
+     * @return true if kaffe is on the current classpath
+     */
+    public static boolean isAvailable() {
+        return getRmicClass() != null;
+    }
+
+    /**
+     * tries to load Kaffe RMIC and falls back to the older class name
+     * if necessary.
+     *
+     * @return null if neither class can get loaded.
+     */
+    private static Class getRmicClass() {
+        try {
+            return Class.forName(RMIC_CLASSNAME);
+        } catch (ClassNotFoundException cnfe) {
+            try {
+                return Class.forName(OLD_RMIC_CLASSNAME);
+            } catch (ClassNotFoundException cnfe2) {
             }
         }
+        return null;
     }
 }
