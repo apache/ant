60a709d594d002a017dddb2d2cd9c141f250de94
Peter Reilly <peterreilly@apache.org>
2005-01-21 17:40:30 +0000

    javadoc
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@277425 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/util/TeeOutputStream.java
+++ src/main/org/apache/tools/ant/util/TeeOutputStream.java
@@ -1,5 +1,5 @@
 /*
- * Copyright  2003-2004 The Apache Software Foundation
+ * Copyright  2003-2005 The Apache Software Foundation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -28,31 +28,64 @@ public class TeeOutputStream extends OutputStream {
     private OutputStream left;
     private OutputStream right;
 
+    /**
+     * Constructor for TeeOutputStream.
+     * @param left one of the output streams.
+     * @param right the other output stream.
+     */
     public TeeOutputStream(OutputStream left, OutputStream right) {
         this.left = left;
         this.right = right;
     }
 
+    /**
+     * Close both output streams.
+     * @throws IOException on error.
+     */
     public void close() throws IOException {
-        left.close();
-        right.close();
+        try {
+            left.close();
+        } finally {
+            right.close();
+        }
     }
 
+    /**
+     * Flush both output streams.
+     * @throws IOException on error
+     */
     public void flush() throws IOException {
         left.flush();
         right.flush();
     }
 
+    /**
+     * Write a byte array to both output streams.
+     * @param b an array of bytes.
+     * @throws IOException on error.
+     */
     public void write(byte[] b) throws IOException {
         left.write(b);
         right.write(b);
     }
 
+    /**
+     * Write a byte array to both output streams.
+     * @param b     the data.
+     * @param off   the start offset in the data.
+     * @param len   the number of bytes to write.
+     * @throws IOException on error.
+     */
     public void write(byte[] b, int off, int len) throws IOException {
         left.write(b, off, len);
         right.write(b, off, len);
     }
 
+    /**
+     * Write a byte to both output streams.
+     * @param b the byte to write.
+     * @throws IOException on error.
+     */
     public void write(int b) throws IOException {
         left.write(b);
         right.write(b);
