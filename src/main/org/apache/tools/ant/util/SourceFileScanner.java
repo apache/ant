19ec6ab674efd50161b35620badde318f1b8fa82
Peter Reilly <peterreilly@apache.org>
2005-01-24 14:12:40 +0000

    javadoc
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@277437 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/util/SourceFileScanner.java
+++ src/main/org/apache/tools/ant/util/SourceFileScanner.java
@@ -50,12 +50,13 @@ public class SourceFileScanner implements ResourceFactory {
      * Restrict the given set of files to those that are newer than
      * their corresponding target files.
      *
-     * @param files   the original set of files
-     * @param srcDir  all files are relative to this directory
+     * @param files   the original set of files.
+     * @param srcDir  all files are relative to this directory.
      * @param destDir target files live here. if null file names
      *                returned by the mapper are assumed to be absolute.
      * @param mapper  knows how to construct a target file names from
      *                source file names.
+     * @return an array of filenames.
      */
     public String[] restrict(String[] files, File srcDir, File destDir,
                              FileNameMapper mapper) {
@@ -67,14 +68,15 @@ public class SourceFileScanner implements ResourceFactory {
      * Restrict the given set of files to those that are newer than
      * their corresponding target files.
      *
-     * @param files   the original set of files
-     * @param srcDir  all files are relative to this directory
+     * @param files   the original set of files.
+     * @param srcDir  all files are relative to this directory.
      * @param destDir target files live here. if null file names
      *                returned by the mapper are assumed to be absolute.
      * @param mapper  knows how to construct a target file names from
      *                source file names.
      * @param granularity The number of milliseconds leeway to give
      *                    before deciding a target is out of date.
+     * @return an array of filenames.
      *
      * @since Ant 1.6.2
      */
@@ -104,9 +106,16 @@ public class SourceFileScanner implements ResourceFactory {
     }
 
     /**
-     * Convinience layer on top of restrict that returns the source
+     * Convenience layer on top of restrict that returns the source
      * files as File objects (containing absolute paths if srcDir is
      * absolute).
+     * @param files   the original set of files.
+     * @param srcDir  all files are relative to this directory.
+     * @param destDir target files live here. if null file names
+     *                returned by the mapper are assumed to be absolute.
+     * @param mapper  knows how to construct a target file names from
+     *                source file names.
+     * @return an array of files.
      */
     public File[] restrictAsFiles(String[] files, File srcDir, File destDir,
                                   FileNameMapper mapper) {
@@ -119,6 +128,15 @@ public class SourceFileScanner implements ResourceFactory {
      * files as File objects (containing absolute paths if srcDir is
      * absolute).
      *
+     * @param files   the original set of files.
+     * @param srcDir  all files are relative to this directory.
+     * @param destDir target files live here. if null file names
+     *                returned by the mapper are assumed to be absolute.
+     * @param mapper  knows how to construct a target file names from
+     *                source file names.
+     * @param granularity The number of milliseconds leeway to give
+     *                    before deciding a target is out of date.
+     * @return an array of files.
      * @since Ant 1.6.2
      */
     public File[] restrictAsFiles(String[] files, File srcDir, File destDir,
@@ -132,9 +150,9 @@ public class SourceFileScanner implements ResourceFactory {
     }
 
     /**
-     * returns resource information for a file at destination
-     * @param name relative path of file at destination
-     * @return data concerning a file whose relative path to destDir is name
+     * Returns resource information for a file at destination.
+     * @param name relative path of file at destination.
+     * @return data concerning a file whose relative path to destDir is name.
      *
      * @since Ant 1.5.2
      */
