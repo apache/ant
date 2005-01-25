0e49304e7055bb0636fdc13fec52fc983d2b1c1d
Peter Reilly <peterreilly@apache.org>
2005-01-25 15:31:36 +0000

    remove unused import
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@277455 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/filters/StringInputStream.java
+++ src/main/org/apache/tools/ant/filters/StringInputStream.java
@@ -1,5 +1,5 @@
 /*
- * Copyright  2002-2004 The Apache Software Foundation
+ * Copyright  2002-2005 The Apache Software Foundation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -16,8 +16,6 @@
  */
 package org.apache.tools.ant.filters;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.StringReader;
 
 import org.apache.tools.ant.util.ReaderInputStream;
