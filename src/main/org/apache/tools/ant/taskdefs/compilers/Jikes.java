2159de4a8f75f63c1167f7ca71dd9c592a472446
Stefan Bodewig <bodewig@apache.org>
2005-02-04 15:41:39 +0000

    logging of command line was incomplete
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@277596 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/taskdefs/compilers/Jikes.java
+++ src/main/org/apache/tools/ant/taskdefs/compilers/Jikes.java
@@ -1,5 +1,5 @@
 /*
- * Copyright  2001-2004 The Apache Software Foundation
+ * Copyright  2001-2005 The Apache Software Foundation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -195,13 +195,14 @@ public class Jikes extends DefaultCompilerAdapter {
         addCurrentCompilerArgs(cmd);
 
         int firstFileName = cmd.size();
-        logAndAddFilesToCompile(cmd);
 
         Path boot = getBootClassPath();
         if (boot.size() > 0) {
             cmd.createArgument().setValue("-bootclasspath");
             cmd.createArgument().setPath(boot);
         }
+
+        logAndAddFilesToCompile(cmd);
 
         return
             executeExternalCompile(cmd.getCommandline(), firstFileName) == 0;
