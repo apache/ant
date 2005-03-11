0641431ea7db7115eb04c96c69c7923acbfc7385
Peter Reilly <peterreilly@apache.org>
2005-03-11 11:10:30 +0000

    spacing
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@277898 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/Diagnostics.java
+++ src/main/org/apache/tools/ant/Diagnostics.java
@@ -281,7 +281,7 @@ public final class Diagnostics {
         String home = System.getProperty(Launcher.USER_HOMEDIR);
         out.println("user.home: " + home);
         File libDir = new File(home,
-                Launcher.ANT_PRIVATEDIR+File.separator + Launcher.ANT_PRIVATELIB);
+                Launcher.ANT_PRIVATEDIR + File.separator + Launcher.ANT_PRIVATELIB);
         File[] libs = listJarFiles(libDir);
         printLibraries(libs, out);
     }
