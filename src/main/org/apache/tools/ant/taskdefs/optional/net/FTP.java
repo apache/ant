90179bc6486079c92ebad1a26cc9b24251ead934
Jacobus Martinus Kruithof <jkf@apache.org>
2005-03-14 19:20:14 +0000

    PR: 28797
    FTP list command could crash with an ArrayIndexOutOfBoundsException.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/branches/ANT_16_BRANCH@277960 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/taskdefs/optional/net/FTP.java
+++ src/main/org/apache/tools/ant/taskdefs/optional/net/FTP.java
@@ -1842,17 +1842,17 @@ public class FTP
      * @throws BuildException in unknown circumstances
      */
     protected void listFile(FTPClient ftp, BufferedWriter bw, String filename)
-         throws IOException, BuildException {
+            throws IOException, BuildException {
         if (verbose) {
             log("listing " + filename);
         }
+        FTPFile[] ftpfiles = ftp.listFiles(resolveFile(filename));
 
-        FTPFile ftpfile = ftp.listFiles(resolveFile(filename))[0];
-
-        bw.write(ftpfile.toString());
-        bw.newLine();
-
-        transferred++;
+        if (ftpfiles != null && ftpfiles.length > 0) {
+            bw.write(ftpfiles[0].toString());
+            bw.newLine();
+            transferred++;
+        }
     }
 
 
