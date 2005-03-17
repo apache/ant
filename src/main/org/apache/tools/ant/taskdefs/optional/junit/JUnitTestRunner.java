5d55ff30081b66a52c7df2f97adbcdc4c96372a7
Stefan Bodewig <bodewig@apache.org>
2005-03-17 08:13:56 +0000

    merge
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/branches/ANT_16_BRANCH@278000 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/taskdefs/optional/junit/JUnitTestRunner.java
+++ src/main/org/apache/tools/ant/taskdefs/optional/junit/JUnitTestRunner.java
@@ -1,5 +1,5 @@
 /*
- * Copyright  2000-2004 The Apache Software Foundation
+ * Copyright  2000-2005 The Apache Software Foundation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -112,6 +112,7 @@ public class JUnitTestRunner implements TestListener {
                 "junit.awtui.TestRunner",
                 "junit.textui.TestRunner",
                 "java.lang.reflect.Method.invoke(",
+                "sun.reflect.",
                 "org.apache.tools.ant."
         };
 
