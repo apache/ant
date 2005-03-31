40e353b593ae9ac5b54df6abba78cf5c952b8091
Stefan Bodewig <bodewig@apache.org>
2005-03-31 06:51:05 +0000

    Merge fix for 24281
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/branches/ANT_16_BRANCH@278095 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/taskdefs/cvslib/CvsTagDiff.java
+++ src/main/org/apache/tools/ant/taskdefs/cvslib/CvsTagDiff.java
@@ -30,7 +30,12 @@ import java.util.StringTokenizer;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.taskdefs.AbstractCvsTask;
+import org.apache.tools.ant.util.DOMElementWriter;
+import org.apache.tools.ant.util.DOMUtils;
 import org.apache.tools.ant.util.FileUtils;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 /**
  * Examines the output of cvs rdiff between two tags.
@@ -64,6 +69,15 @@ import org.apache.tools.ant.util.FileUtils;
  * @ant.task name="cvstagdiff"
  */
 public class CvsTagDiff extends AbstractCvsTask {
+
+    /**
+     * Used to create the temp file for cvs log
+     */
+    private static final FileUtils FILE_UTILS = FileUtils.getFileUtils();
+
+    /** stateless helper for writing the XML document */
+    private static final DOMElementWriter DOM_WRITER = new DOMElementWriter();
+
     /**
      * Token to identify the word file in the rdiff log
      */
@@ -280,10 +294,6 @@ public class CvsTagDiff extends AbstractCvsTask {
                     }
 
                     if ((index = line.indexOf(FILE_IS_NEW)) != -1) {
-//CVS 1.11
-//File apps/websphere/lib/something.jar is new; current revision 1.2
-//CVS 1.11.9
-//File apps/websphere/lib/something.jar is new; cvstag_2003_11_03_2 revision 1.2
                         // it is a new file
                         // set the revision but not the prevrevision
                         String filename = line.substring(0, index);
@@ -355,26 +365,27 @@ public class CvsTagDiff extends AbstractCvsTask {
             PrintWriter writer = new PrintWriter(
                                      new OutputStreamWriter(output, "UTF-8"));
             writer.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-            writer.print("<tagdiff ");
+            Document doc = DOMUtils.newDocument();
+            Element root = doc.createElement("tagdiff");
             if (mystartTag != null) {
-                writer.print("startTag=\"" + mystartTag + "\" ");
+                root.setAttribute("startTag", mystartTag);
             } else {
-                writer.print("startDate=\"" + mystartDate + "\" ");
+                root.setAttribute("startDate", mystartDate);
             }
             if (myendTag != null) {
-                writer.print("endTag=\"" + myendTag + "\" ");
+                root.setAttribute("endTag", myendTag);
             } else {
-                writer.print("endDate=\"" + myendDate + "\" ");
+                root.setAttribute("endDate", myendDate);
             }
 
-            writer.print("cvsroot=\"" + getCvsRoot() + "\" ");
-            writer.print("package=\"" + mypackage + "\" ");
-
-            writer.println(">");
+            root.setAttribute("cvsroot", getCvsRoot());
+            root.setAttribute("package", mypackage);
+            DOM_WRITER.openElement(root, writer, 0, "\t");
+            writer.println();
             for (int i = 0, c = entries.length; i < c; i++) {
-                writeTagEntry(writer, entries[i]);
+                writeTagEntry(doc, writer, entries[i]);
             }
-            writer.println("</tagdiff>");
+            DOM_WRITER.closeElement(root, writer, 0, "\t", true);
             writer.flush();
             writer.close();
         } catch (UnsupportedEncodingException uee) {
@@ -395,23 +406,24 @@ public class CvsTagDiff extends AbstractCvsTask {
     /**
      * Write a single entry to the given writer.
      *
+     * @param doc Document used to create elements.
      * @param writer a <code>PrintWriter</code> value
      * @param entry a <code>CvsTagEntry</code> value
      */
-    private void writeTagEntry(PrintWriter writer, CvsTagEntry entry) {
-        writer.println("\t<entry>");
-        writer.println("\t\t<file>");
-        writer.println("\t\t\t<name>" + entry.getFile() + "</name>");
+    private void writeTagEntry(Document doc, PrintWriter writer,
+                               CvsTagEntry entry)
+        throws IOException {
+        Element ent = doc.createElement("entry");
+        Element f = DOMUtils.createChildElement(ent, "file");
+        DOMUtils.appendCDATAElement(f, "name", entry.getFile());
         if (entry.getRevision() != null) {
-            writer.println("\t\t\t<revision>" + entry.getRevision()
-                           + "</revision>");
+            DOMUtils.appendTextElement(f, "revision", entry.getRevision());
         }
         if (entry.getPreviousRevision() != null) {
-            writer.println("\t\t\t<prevrevision>"
-                           + entry.getPreviousRevision() + "</prevrevision>");
+            DOMUtils.appendTextElement(f, "prevrevision",
+                                       entry.getPreviousRevision());
         }
-        writer.println("\t\t</file>");
-        writer.println("\t</entry>");
+        DOM_WRITER.write(ent, writer, 1, "\t");
     }
 
     /**
