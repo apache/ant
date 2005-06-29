8fbf9a4b9a81ca815e9d44060fbe15d37dd449e6
Peter Reilly <peterreilly@apache.org>
2005-06-29 14:58:10 +0000

    checkstyle - line length
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@278442 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/taskdefs/optional/i18n/Translate.java
+++ src/main/org/apache/tools/ant/taskdefs/optional/i18n/Translate.java
@@ -548,20 +548,21 @@ public class Translate extends MatchingTask {
                             while (startIndex >= 0
                                 && (startIndex + startToken.length()) <= line.length()) {
                                 // the new value, this needs to be here
-                                // because it is required to calculate the next position to search from
-                                // at the end of the loop
+                                // because it is required to calculate the next position to
+                                // search from at the end of the loop
                                 String replace = null;
 
                                 // we found a starttoken, is there an endtoken following?
                                 // start at token+tokenlength because start and end
                                 // token may be indentical
-                                int endIndex = line.indexOf(endToken, startIndex + startToken.length());
+                                int endIndex = line.indexOf(
+                                    endToken, startIndex + startToken.length());
                                 if (endIndex < 0) {
                                     startIndex += 1;
                                 } else {
                                     // grab the token
-                                    String token
-                                        = line.substring(startIndex + startToken.length(), endIndex);
+                                    String token = line.substring(
+                                        startIndex + startToken.length(), endIndex);
 
                                     // If there is a white space or = or :, then
                                     // it isn't to be treated as a valid key.
