b4b0a55f5afd236b3933cd1b9c15e9bf98a39eb5
Jan Materne <jhm@apache.org>
2005-10-29 10:03:31 +0000

    Revert my previous change and place a note why we leave apply=Transform.class.
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@329387 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/taskdefs/Transform.java
+++ src/main/org/apache/tools/ant/taskdefs/Transform.java
@@ -1,5 +1,5 @@
 /*
- * Copyright  2000-2002,2004 The Apache Software Foundation
+ * Copyright  2000-2002,2004,2005 The Apache Software Foundation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -19,6 +19,9 @@ package org.apache.tools.ant.taskdefs;
 
 /**
  * Has been merged into ExecuteOn, empty class for backwards compatibility.
+ * We leave that in case that external programs access this class direclty,
+ * for example via
+ *   <tt> Transform tr = (Transform) getProject().createTask("apply") </tt>
  *
  * @ant.task ignore="true"
  */
