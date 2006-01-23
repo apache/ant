fa78f1b3b09f395ad0b32c87e25829f669c91643
Matthew Jason Benson <mbenson@apache.org>
2006-01-23 15:31:38 +0000

    fix ANTLRTest in Gump (extra output files created by version 2.7.6 invalidate assertions made in JUnit.  Made assertions more explicit and moved into the test buildfile.
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@371555 13f79535-47bb-0310-9956-ffa450edef68

--- src/testcases/org/apache/tools/ant/taskdefs/optional/ANTLRTest.java
+++ src/testcases/org/apache/tools/ant/taskdefs/optional/ANTLRTest.java
@@ -57,9 +57,6 @@ public class ANTLRTest extends BuildFileTest {
 
     public void test3() {
         executeTarget("test3");
-        File outputDirectory = new File(System.getProperty("root"), TASKDEFS_DIR + "antlr.tmp");
-        String[] calcFiles = outputDirectory.list(new CalcFileFilter());
-        assertEquals(5, calcFiles.length);
     }
 
     public void test4() {
