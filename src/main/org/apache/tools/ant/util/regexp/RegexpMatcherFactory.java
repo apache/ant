f433fd17127d5ae200d26100b5897360a8f3812e
Matthew Jason Benson <mbenson@apache.org>
2006-04-20 15:09:41 +0000

    remove extra space
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@395614 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/util/regexp/RegexpMatcherFactory.java
+++ src/main/org/apache/tools/ant/util/regexp/RegexpMatcherFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright  2000-2005 The Apache Software Foundation
+ * Copyright  2000-2006 The Apache Software Foundation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -104,7 +104,7 @@ public class RegexpMatcherFactory {
         throws BuildException 
     {
         return (RegexpMatcher) ClasspathUtils.newInstance(className,
-                RegexpMatcherFactory.class.getClassLoader(), RegexpMatcher .class);
+                RegexpMatcherFactory.class.getClassLoader(), RegexpMatcher.class);
     }
 
     /**
