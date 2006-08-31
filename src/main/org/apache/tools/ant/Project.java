4e5a656cdfa507382d15e04e131157985375201a
Matthew Jason Benson <mbenson@apache.org>
2006-08-31 15:37:27 +0000

    Strike lying comment.
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@438953 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/Project.java
+++ src/main/org/apache/tools/ant/Project.java
@@ -1,5 +1,5 @@
 /*
- * Copyright  2000-2006 The Apache Software Foundation
+ * Copyright 2000-2006 The Apache Software Foundation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -1804,7 +1804,7 @@ public class Project implements ResourceFactory {
      * Add a reference to the project.
      *
      * @param referenceName The name of the reference. Must not be <code>null</code>.
-     * @param value The value of the reference. Must not be <code>null</code>.
+     * @param value The value of the reference.
      */
     public void addReference(String referenceName, Object value) {
         synchronized (references) {
