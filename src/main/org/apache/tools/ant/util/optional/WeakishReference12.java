017b808e7f615b3a1673108885337c3f0639a555
Peter Reilly <peterreilly@apache.org>
2006-11-15 00:08:00 +0000

    fix javadoc
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@475073 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/util/optional/WeakishReference12.java
+++ src/main/org/apache/tools/ant/util/optional/WeakishReference12.java
@@ -24,7 +24,7 @@ import org.apache.tools.ant.util.WeakishReference;
  * This is a reference that really is is Weak, as it uses the
  * appropriate java.lang.ref class.
  * @deprecated since 1.7.
- *             Just use WeakReference directly.
+ *             Just use {@link java.lang.ref.WeakReference} directly.
  * Note that in ant1.7 is parent was changed to extend HardReference.
  * This is because the latter has access to the (package scoped)
  * WeakishReference(Object) constructor, and both that and this are thin
@@ -36,7 +36,7 @@ public class WeakishReference12 extends WeakishReference.HardReference  {
     /**
      * create a new soft reference, which is bound to a
      * Weak reference inside
-     * @param reference
+     * @param reference the object to reference.
      * @see java.lang.ref.WeakReference
      */
     public WeakishReference12(Object reference) {
