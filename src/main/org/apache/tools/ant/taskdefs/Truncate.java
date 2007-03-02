9e500d883f757824c2aeb136fb14e602ddfa1df1
Matthew Jason Benson <mbenson@apache.org>
2007-03-02 19:32:10 +0000

    oops unnecessary import
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@513903 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/taskdefs/Truncate.java
+++ src/main/org/apache/tools/ant/taskdefs/Truncate.java
@@ -25,7 +25,6 @@ import java.util.Iterator;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.Task;
-import org.apache.tools.ant.types.EnumeratedAttribute;
 import org.apache.tools.ant.types.Path;
 import org.apache.tools.ant.types.ResourceCollection;
 import org.apache.tools.ant.types.resources.FileResource;
