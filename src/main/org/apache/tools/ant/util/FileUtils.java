e52412cab9febc288122fc52080a1fc54d84517b
Antoine Levy-Lambert <antoine@apache.org>
2007-03-29 12:21:36 +0000

    restore detailed import lines
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@523683 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/util/FileUtils.java
+++ src/main/org/apache/tools/ant/util/FileUtils.java
@@ -18,7 +18,15 @@
 
 package org.apache.tools.ant.util;
 
-import java.io.*;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.DecimalFormat;
