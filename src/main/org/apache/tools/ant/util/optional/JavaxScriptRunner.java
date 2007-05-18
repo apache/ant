172b90cdaef43160dcf7488edbfd4ca1bb638a5d
Peter Reilly <peterreilly@apache.org>
2007-05-18 10:31:34 +0000

    fx can be used as well as FX for openjfx
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@539370 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/util/optional/JavaxScriptRunner.java
+++ src/main/org/apache/tools/ant/util/optional/JavaxScriptRunner.java
@@ -90,7 +90,7 @@ public class JavaxScriptRunner extends ScriptRunnerBase {
             for (Iterator i = getBeans().keySet().iterator(); i.hasNext();) {
                 String key = (String) i.next();
                 Object value = getBeans().get(key);
-                if ("FX".equals(getLanguage())) {
+                if ("FX".equalsIgnoreCase(getLanguage())) {
                     engine.invoke(
                         "put", String.class, key
                         + ":" + value.getClass().getName(),
