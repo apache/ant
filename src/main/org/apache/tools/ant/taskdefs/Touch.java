b0df67412caff06226d505d6525288808363b2a3
Matthew Jason Benson <mbenson@apache.org>
2007-11-06 18:30:37 +0000

    merge
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/branches/ANT_17_BRANCH@592515 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/taskdefs/Touch.java
+++ src/main/org/apache/tools/ant/taskdefs/Touch.java
@@ -15,7 +15,6 @@
  *  limitations under the License.
  *
  */
-
 package org.apache.tools.ant.taskdefs;
 
 import java.io.File;
@@ -85,7 +84,7 @@ public class Touch extends Task {
     private long millis = -1;
     private String dateTime;
     private Vector filesets = new Vector();
-    private Union resources = new Union();
+    private Union resources;
     private boolean dateTimeConfigured;
     private boolean mkdirs;
     private boolean verbose = true;
@@ -212,7 +211,8 @@ public class Touch extends Task {
      * @param rc the collection to add.
      * @since Ant 1.7
      */
-    public void add(ResourceCollection rc) {
+    public synchronized void add(ResourceCollection rc) {
+        resources = resources == null ? new Union() : resources;
         resources.add(rc);
     }
 
@@ -222,7 +222,7 @@ public class Touch extends Task {
      * @since Ant 1.6.3
      */
     protected synchronized void checkConfiguration() throws BuildException {
-        if (file == null && resources.size() == 0) {
+        if (file == null && resources == null) {
             throw new BuildException("Specify at least one source"
                                    + "--a file or resource collection.");
         }
@@ -285,6 +285,9 @@ public class Touch extends Task {
         if (file != null) {
             touch(new FileResource(file.getParentFile(), file.getName()),
                   defaultTimestamp);
+        }
+        if (resources == null) {
+            return;
         }
         // deal with the resource collections
         Iterator iter = resources.iterator();
