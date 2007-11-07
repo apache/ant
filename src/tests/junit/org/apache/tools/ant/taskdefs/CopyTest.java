3ff20b066ccfc5397e2409ee57e404fa178fb9d8
Jan Materne <jhm@apache.org>
2007-11-07 18:24:08 +0000

    use static methods in a static way
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@592844 13f79535-47bb-0310-9956-ffa450edef68

--- src/tests/junit/org/apache/tools/ant/taskdefs/CopyTest.java
+++ src/tests/junit/org/apache/tools/ant/taskdefs/CopyTest.java
@@ -181,7 +181,7 @@ public class CopyTest extends BuildFileTest {
         File file1 = new File(getProjectDir(), getProject().getProperty("to.dir")+"/fileNR.txt");
         assertTrue(file1.exists());
         try {
-            String file1Content = FILE_UTILS.readFully(new FileReader(file1));
+            String file1Content = FileUtils.readFully(new FileReader(file1));
             assertEquals("This is file 42", file1Content);
         } catch (IOException e) {
             // no-op: not a real business error
