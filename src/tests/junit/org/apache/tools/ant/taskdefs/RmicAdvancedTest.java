4a73a85b719bd8445dc1babad19bae2003644b57
Kevin Jackson <kevj@apache.org>
2007-11-12 03:15:32 +0000

    -comment WLrmic tests
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@594016 13f79535-47bb-0310-9956-ffa450edef68

--- src/tests/junit/org/apache/tools/ant/taskdefs/RmicAdvancedTest.java
+++ src/tests/junit/org/apache/tools/ant/taskdefs/RmicAdvancedTest.java
@@ -85,18 +85,18 @@ public class RmicAdvancedTest extends BuildFileTest {
     public void testKaffe() throws Exception {
         executeTarget("testKaffe");
     }
-
+    // WLrmic tests don't work
     /**
      * test weblogic
      */
-    public void testWlrmic() throws Exception {
+    public void XtestWlrmic() throws Exception {
         executeTarget("testWlrmic");
     }
 
     /**
      *  test weblogic's stripping of -J args
      */
-    public void testWlrmicJArg() throws Exception {
+    public void XtestWlrmicJArg() throws Exception {
         executeTarget("testWlrmicJArg");
     }
 
