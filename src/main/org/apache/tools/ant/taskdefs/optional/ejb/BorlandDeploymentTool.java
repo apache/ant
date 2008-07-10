d8ce03fe18c2cdef2f1c75c7727967f31352408c
Stefan Bodewig <bodewig@apache.org>
2008-07-10 15:03:11 +0000

    properly deal with multiple arguments to java2iioparams.  PR 27444.
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@675605 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/taskdefs/optional/ejb/BorlandDeploymentTool.java
+++ src/main/org/apache/tools/ant/taskdefs/optional/ejb/BorlandDeploymentTool.java
@@ -415,7 +415,7 @@ public class BorlandDeploymentTool extends GenericDeploymentTool
 
         if (java2iioparams != null) {
             log("additional  " + java2iioparams + " to java2iiop ", 0);
-            commandline.createArgument().setValue(java2iioparams);
+            commandline.createArgument().setLine(java2iioparams);
         }
 
 
