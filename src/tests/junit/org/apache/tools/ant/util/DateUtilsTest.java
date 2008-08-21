49324dc8c3121ee4f6c3f7bea532469db98b5c0c
Stefan Bodewig <bodewig@apache.org>
2008-08-21 13:13:30 +0000

    testcase for PR 44659
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@687739 13f79535-47bb-0310-9956-ffa450edef68

--- src/tests/junit/org/apache/tools/ant/util/DateUtilsTest.java
+++ src/tests/junit/org/apache/tools/ant/util/DateUtilsTest.java
@@ -42,6 +42,12 @@ public class DateUtilsTest extends TestCase {
         assertEquals("2 minutes 1 second", text);
     }
 
+    // https://issues.apache.org/bugzilla/show_bug.cgi?id=44659
+    public void XtestLongElapsedTime(){
+        assertEquals("2926 minutes 13 seconds",
+                     DateUtils.formatElapsedTime(1000 * 175573));
+    }
+
     public void testDateTimeISO(){
         TimeZone timeZone = TimeZone.getTimeZone("GMT+1");
         Calendar cal = Calendar.getInstance(timeZone);
