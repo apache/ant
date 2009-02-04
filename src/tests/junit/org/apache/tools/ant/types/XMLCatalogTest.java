b27fa7d06fb6c98d3a12874e8d8cba65fb17bc40
Stefan Bodewig <bodewig@apache.org>
2009-02-04 04:07:25 +0000

    indentation changes only
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@740597 13f79535-47bb-0310-9956-ffa450edef68

--- src/tests/junit/org/apache/tools/ant/types/XMLCatalogTest.java
+++ src/tests/junit/org/apache/tools/ant/types/XMLCatalogTest.java
@@ -71,33 +71,33 @@ public class XMLCatalogTest extends TestCase {
         catalog = newCatalog();
     }
 
-   public void tearDown() {
-      project = null;
-      catalog = null;
-   }
+    public void tearDown() {
+        project = null;
+        catalog = null;
+    }
 
-   public void testEmptyCatalog() {
-       try {
-           InputSource result = catalog.resolveEntity("PUBLIC ID ONE",
-                                                      "i/dont/exist.dtd");
-           assertNull("Empty catalog should return null", result);
-       } catch (Exception e) {
-           fail("resolveEntity() failed!" + e.toString());
-       }
+    public void testEmptyCatalog() {
+        try {
+            InputSource result = catalog.resolveEntity("PUBLIC ID ONE",
+                                                       "i/dont/exist.dtd");
+            assertNull("Empty catalog should return null", result);
+        } catch (Exception e) {
+            fail("resolveEntity() failed!" + e.toString());
+        }
 
-       try {
-           Source result = catalog.resolve("i/dont/exist.dtd", null);
-           String expected = toURLString(new File(project.getBaseDir() +
-                                                  "/i/dont/exist.dtd"));
-           String resultStr =
-               fileURLPartWithoutLeadingSlashes((SAXSource)result);
-           assertTrue("Empty catalog should return input with a system ID like "
-                      + expected + " but was " + resultStr,
-                      expected.endsWith(resultStr));
-       } catch (Exception e) {
-           fail("resolve() failed!" + e.toString());
-       }
-   }
+        try {
+            Source result = catalog.resolve("i/dont/exist.dtd", null);
+            String expected = toURLString(new File(project.getBaseDir() +
+                                                   "/i/dont/exist.dtd"));
+            String resultStr =
+                fileURLPartWithoutLeadingSlashes((SAXSource)result);
+            assertTrue("Empty catalog should return input with a system ID like "
+                       + expected + " but was " + resultStr,
+                       expected.endsWith(resultStr));
+        } catch (Exception e) {
+            fail("resolve() failed!" + e.toString());
+        }
+    }
 
     private static String fileURLPartWithoutLeadingSlashes(SAXSource result)
         throws MalformedURLException {
