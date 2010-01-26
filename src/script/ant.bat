c16ac4046a1780e1f7a9a0dc7322761aba28db05
Antoine Levy-Lambert <antoine@apache.org>
2010-01-26 17:41:27 +0000

    fix for bug 48186 ant.bat file ignores -noclasspath if it is the first parameter supplied by Ken Southerland.
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@903337 13f79535-47bb-0310-9956-ffa450edef68

--- src/script/ant.bat
+++ src/script/ant.bat
@@ -59,9 +59,6 @@ if "%CLASSPATH%"=="" set _USE_CLASSPATH=no
 
 rem Slurp the command line arguments. This loop allows for an unlimited number
 rem of arguments (up to the command line limit, anyway).
-set ANT_CMD_LINE_ARGS=%1
-if ""%1""=="""" goto doneStart
-shift
 :setupArgs
 if ""%1""=="""" goto doneStart
 if ""%1""==""-noclasspath"" goto clearclasspath
