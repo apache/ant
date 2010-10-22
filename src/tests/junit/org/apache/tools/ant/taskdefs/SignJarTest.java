c735a5cc4880fc93861da18f06b21e7c2d057a53
Stefan Bodewig <bodewig@apache.org>
2010-10-22 14:08:09 +0000

    move bulk of signjar tests to AntUnit tests
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@1026334 13f79535-47bb-0310-9956-ffa450edef68

--- src/tests/junit/org/apache/tools/ant/taskdefs/SignJarTest.java
+++ src/tests/junit/org/apache/tools/ant/taskdefs/SignJarTest.java
@@ -51,10 +51,6 @@ public class SignJarTest extends BuildFileTest {
     private boolean isOffline() {
         return Boolean.getBoolean("offline");
     }
-    public void testBasicSigning() {
-        executeTarget("basic");
-    }
-
     public void testSigFile() {
         executeTarget("sigfile");
         SignJarChild sj = new SignJarChild();
@@ -91,10 +87,6 @@ public class SignJarTest extends BuildFileTest {
         }
     }
 
-    public void testMaxMemory() {
-        executeTarget("maxmemory");
-    }
-
     public void testURLKeystoreFile() {
         executeTarget("urlKeystoreFile");
     }
@@ -105,104 +97,6 @@ public class SignJarTest extends BuildFileTest {
         }
     }
 
-    public void testPreserveLastModified() {
-        executeTarget("preserveLastModified");
-    }
-
-    public void testFileset() {
-        executeTarget("testFileset");
-    }
-
-    public void testFilesetAndJar() {
-        executeTarget("testFilesetAndJar");
-    }
-
-    public void testFilesetAndSignedJar() {
-        expectBuildExceptionContaining("testFilesetAndSignedJar",
-                "incompatible attributes",
-                SignJar.ERROR_SIGNEDJAR_AND_PATHS);
-    }
-
-    public void testPath() {
-        executeTarget("testPath");
-    }
-
-    public void testPathAndJar() {
-        executeTarget("testPathAndJar");
-    }
-
-    public void testPathAndSignedJar() {
-        expectBuildExceptionContaining("testPathAndSignedJar",
-                "incompatible attributes",
-                SignJar.ERROR_SIGNEDJAR_AND_PATHS);
-    }
-
-    public void testSignedJar() {
-        executeTarget("testSignedJar");
-    }
-
-    public void testDestDir() {
-        executeTarget("testDestDir");
-    }
-
-    public void testDestDirAndSignedJar() {
-        expectBuildExceptionContaining("testFilesetAndSignedJar",
-                "incompatible attributes",
-                SignJar.ERROR_SIGNEDJAR_AND_PATHS);
-    }
-
-    public void testDestDirAndSignedJar2() {
-        expectBuildExceptionContaining("testPathAndSignedJar",
-                "incompatible attributes",
-                SignJar.ERROR_SIGNEDJAR_AND_PATHS);
-    }
-
-    public void testDestDirFileset() {
-        executeTarget("testDestDirFileset");
-    }
-
-    public void testMapperFileset() {
-        executeTarget("testMapperFileset");
-    }
-
-    public void testDestDirPath() {
-        executeTarget("testDestDirPath");
-    }
-
-    public void testMapperPath() {
-        executeTarget("testMapperPath");
-    }
-
-    public void testMapperNoDest() {
-        expectBuildExceptionContaining("testMapperNoDest",
-                "two mappers",
-                SignJar.ERROR_MAPPER_WITHOUT_DEST);
-    }
-
-    public void testTwoMappers() {
-        expectBuildExceptionContaining("testTwoMappers",
-                "two mappers",
-                SignJar.ERROR_TOO_MANY_MAPPERS);
-    }
-
-    public void testNoAlias() {
-        expectBuildExceptionContaining("testNoAlias",
-                "no alias",
-                SignJar.ERROR_NO_ALIAS);
-    }
-
-    public void testNoFiles() {
-        expectBuildExceptionContaining("testNoFiles",
-                "no files",
-                SignJar.ERROR_NO_SOURCE);
-    }
-
-    public void testNoStorePass() {
-        expectBuildExceptionContaining("testNoStorePass",
-                "no password",
-                SignJar.ERROR_NO_STOREPASS);
-    }
-
     public void testTsaLocalhost() {
         //only test on java1.5+
         if(JavaEnvUtils.getJavaVersionNumber()>=15) {
@@ -210,40 +104,6 @@ public class SignJarTest extends BuildFileTest {
                 "no TSA at localhost:0");
             assertLogContaining("java.net.ConnectException");
         }
-    }
-
-    public void testSysProperty() {
-        executeTarget("testSysProperty");
-    }
-
-    public void testVerifyJar() {
-        executeTarget("testVerifyJar");
-    }
-
-    public void testVerifyNoArgs() {
-        expectBuildExceptionContaining("testVerifyNoArgs",
-                "no args",
-                AbstractJarSignerTask.ERROR_NO_SOURCE);
-    }
-
-    public void testVerifyJarUnsigned() {
-        expectBuildExceptionContaining("testVerifyJarUnsigned",
-                "unsigned JAR file",
-                VerifyJar.ERROR_NO_VERIFY);
-    }
-
-    public void NotestVerifyJarNotInKeystore() {
-        expectBuildExceptionContaining("testVerifyJarNotInKeystore",
-                "signature not in keystore",
-                VerifyJar.ERROR_NO_VERIFY);
-    }
-
-    public void testVerifyFileset() {
-        executeTarget("testVerifyFileset");
-    }
-
-    public void testVerifyPath() {
-        executeTarget("testVerifyPath");
     }
 
 }
