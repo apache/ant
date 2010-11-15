db666cd282c2115820062a038ff179d3680b4336
Nicolas Lalevee <hibou@apache.org>
2010-11-15 16:09:41 +0000

    remove useless import
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@1035336 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/Main.java
+++ src/main/org/apache/tools/ant/Main.java
@@ -20,7 +20,6 @@ package org.apache.tools.ant;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
