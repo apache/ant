0942613299355a4cdeb933243dbc243c78a7c89b
Antoine Levy-Lambert <antoine@apache.org>
2011-01-07 13:49:18 +0000

    update of complete-ant-cmd.pl suggested by Ondra Medeksee http://mail-archives.apache.org/mod_mbox/ant-user/201101.mbox/%3CAANLkTinbngSscXy5nAQJaCRQPcVHJQqRJSyuzdneyene@mail.gmail.com%3E
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@1056313 13f79535-47bb-0310-9956-ffa450edef68

--- src/script/complete-ant-cmd.pl
+++ src/script/complete-ant-cmd.pl
@@ -19,7 +19,7 @@
 #
 # To install for Bash 2.0 or better, add the following to ~/.bashrc:
 # 
-#     $ complete -C complete-ant-cmd ant build.sh
+#     complete -C complete-ant-cmd.pl ant build.sh
 #
 # To install for Z-Shell 2.5 or better, add the following to ~/.zshrc:
 #
@@ -27,7 +27,7 @@
 #         local args_line args
 #         read -l args_line
 #         set -A args $args_line
-#         set -A reply $(COMP_LINE=$args_line complete-ant-cmd ${args[1]} $1)
+#         set -A reply $(COMP_LINE=$args_line complete-ant-cmd.pl ${args[1]} $1)
 #     }
 #     compctl -K ant_complete ant build.sh
 #     
