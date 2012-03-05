26337bb13561b001c2d7892e637961099f1644bf
Jesse N. Glick <jglick@apache.org>
2012-03-05 18:49:30 +0000

    MS_SHOULD_BE_FINAL
    git-svn-id: https://svn.apache.org/repos/asf/ant/core/trunk@1297164 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/tools/ant/types/Path.java
+++ src/main/org/apache/tools/ant/types/Path.java
@@ -67,7 +67,7 @@ public class Path extends DataType implements Cloneable, ResourceCollection {
     // CheckStyle:VisibilityModifier OFF - bc
 
     /** The system classpath as a Path object */
-    public static Path systemClasspath =
+    public static final Path systemClasspath =
         new Path(null, System.getProperty("java.class.path"));
 
 
@@ -76,7 +76,7 @@ public class Path extends DataType implements Cloneable, ResourceCollection {
      *
      * @since Ant 1.6.2
      */
-    public static Path systemBootClasspath =
+    public static final Path systemBootClasspath =
         new Path(null, System.getProperty("sun.boot.class.path"));
 
     private static final Iterator EMPTY_ITERATOR
