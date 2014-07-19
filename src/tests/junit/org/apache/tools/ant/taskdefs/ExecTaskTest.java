9eb4c3a9a00c2062a02a7f064df983625c93a9bd
Stefan Bodewig <bodewig@apache.org>
2014-07-19 20:00:34 +0200

    try to make spawn test pass in Jenkins by using a bigger security margin

--- src/tests/junit/org/apache/tools/ant/taskdefs/ExecTaskTest.java
+++ src/tests/junit/org/apache/tools/ant/taskdefs/ExecTaskTest.java
@@ -48,9 +48,9 @@ public class ExecTaskTest {
     private static final int TIME_TO_WAIT = 1;
     /** maximum time allowed for the build in milliseconds */
     private static final int MAX_BUILD_TIME = 4000;
-    private static final int SECURITY_MARGIN = 2000; // wait 2 second extras
-    // the test failed with 100 ms of margin on cvs.apache.org on August 1st,
-    // 2003
+    private static final int SECURITY_MARGIN = 3000; // wait 3 second extras
+    // the test failed with 100 ms of margin on cvs.apache.org on August 1st, 2003
+    // the test failed with 2 s of margin on Windows Jenkins slaves on during July 2014
 
     /** Utilities used for file operations */
     private static final FileUtils FILE_UTILS = FileUtils.getFileUtils();
@@ -74,7 +74,7 @@ public class ExecTaskTest {
                 false, false);
         // this is guaranteed by FileUtils#createTempFile
         assertTrue("log file not existing", !logFile.exists());
-        // make the spawned process run 4 seconds
+        // make the spawned process run 1 seconds
         myBuild.setTimeToWait(TIME_TO_WAIT);
         myBuild.setLogFile(logFile.getAbsolutePath());
         myBuild.addBuildListener(new MonitoredBuildListener());
