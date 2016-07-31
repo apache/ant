a62d5f9f2a9e329c9b5d233b15b658b4e25f4018
Stefan Bodewig <bodewig@apache.org>
2016-07-31 09:36:41 +0200

    formatting

--- src/tests/junit/org/apache/tools/ant/taskdefs/optional/TraXLiaisonTest.java
+++ src/tests/junit/org/apache/tools/ant/taskdefs/optional/TraXLiaisonTest.java
@@ -1,3 +1,21 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 package org.apache.tools.ant.taskdefs.optional;
 
 import static org.junit.Assert.assertTrue;
@@ -17,24 +35,6 @@ import org.junit.After;
 import org.junit.Assume;
 import org.junit.Test;
 
-/*
- *  Licensed to the Apache Software Foundation (ASF) under one or more
- *  contributor license agreements.  See the NOTICE file distributed with
- *  this work for additional information regarding copyright ownership.
- *  The ASF licenses this file to You under the Apache License, Version 2.0
- *  (the "License"); you may not use this file except in compliance with
- *  the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- */
-
 /**
  * TraX XSLTLiaison testcase
  */
@@ -42,7 +42,7 @@ public class TraXLiaisonTest extends AbstractXSLTLiaisonTest
     implements XSLTLogger {
 
 
-	@After
+    @After
     public void tearDown() {
         File f = new File("xalan2-redirect-out.tmp");
         if (f.exists()) {
@@ -58,11 +58,11 @@ public class TraXLiaisonTest extends AbstractXSLTLiaisonTest
 
     @Test
     public void testXalan2Redirect() throws Exception {
-    	try {
-    		getClass().getClassLoader().loadClass("org.apache.xalan.lib.Redirect");
-    	} catch (Exception exc) {
-    		Assume.assumeNoException("xalan redirect is not on the classpath", exc);
-    	}
+        try {
+            getClass().getClassLoader().loadClass("org.apache.xalan.lib.Redirect");
+        } catch (Exception exc) {
+            Assume.assumeNoException("xalan redirect is not on the classpath", exc);
+        }
         File xsl = getFile("/taskdefs/optional/xalan-redirect-in.xsl");
         liaison.setStylesheet(xsl);
         File out = new File("xalan2-redirect-out-dummy.tmp");
