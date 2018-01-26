5892ab045db81bd74b38ebc494c6ff50005924a7
Stefan Bodewig <bodewig@apache.org>
2018-01-26 20:30:35 +0100

    whitespace

--- src/tests/junit/org/apache/tools/ant/util/JavaEnvUtilsTest.java
+++ src/tests/junit/org/apache/tools/ant/util/JavaEnvUtilsTest.java
@@ -68,9 +68,9 @@ public class JavaEnvUtilsTest {
             String javaHomeParent = FILE_UTILS.normalize(javaHome + "/..").getAbsolutePath();
             assertTrue(j + " is normalized and in the JDK dir", j.startsWith(javaHomeParent));
             if (JavaEnvUtils.isAtLeastJavaVersion(JavaEnvUtils.JAVA_9)) {
-            assertTrue(j + " is normalized and not in the JRE dir", j.startsWith(javaHome));
+                assertTrue(j + " is normalized and not in the JRE dir", j.startsWith(javaHome));
             } else {
-            assertTrue(j + " is normalized and not in the JRE dir", !j.startsWith(javaHome));
+                assertTrue(j + " is normalized and not in the JRE dir", !j.startsWith(javaHome));
             }
         } catch (AssertionError e) {
             // java.home is bogus
