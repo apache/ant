20b6163989fdf4e98d7739fca3cdf9f18a5bdc7a
Maarten Coene <maartenc@apache.org>
2018-04-04 13:26:03 +0200

    Removed unused import.

--- src/tests/junit/org/apache/tools/ant/taskdefs/optional/junit/JUnitTestRunnerTest.java
+++ src/tests/junit/org/apache/tools/ant/taskdefs/optional/junit/JUnitTestRunnerTest.java
@@ -21,7 +21,6 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
-import javafx.scene.control.Control;
 import junit.framework.AssertionFailedError;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
