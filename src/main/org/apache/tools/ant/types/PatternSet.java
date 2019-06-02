15958559a83f424efe99430677a88f67792d8172
twogee <g.grigelionis@gmail.com>
2019-06-02 06:09:20 +0200

    Refer to Java 8 documentation for consistency

--- src/main/org/apache/tools/ant/types/PatternSet.java
+++ src/main/org/apache/tools/ant/types/PatternSet.java
@@ -194,8 +194,8 @@ public class PatternSet extends DataType implements Cloneable {
          *
          * <p>
          * For a list of possible values see
-         * <a href="https://docs.oracle.com/javase/1.5.0/docs/guide/intl/encoding.doc.html">
-         * https://docs.oracle.com/javase/1.5.0/docs/guide/intl/encoding.doc.html</a>.
+         * <a href="https://docs.oracle.com/javase/8/docs/technotes/guides/intl/encoding.doc.html">
+         * https://docs.oracle.com/javase/8/docs/technotes/guides/intl/encoding.doc.html</a>.
          * </p>
          *
          * @param encoding String
