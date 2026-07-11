928be61e77a19328ce13d44e89ddc5f77fcbe4b4
Stefan Bodewig <bodewig@apache.org>
2026-07-11 15:24:06 +0200

    add DateUtils#getNow as helper for reproducible builds
    this mirrors what Tstamp does but we can't use it in Tstamp because
    people may have overridden the protected getNow methods.
    
    This is something I intend to use for reproducible builds of/with Ant
    after 1.10.18 - and want to use in the next release of the CycloneDX
    Antlib.

--- src/main/org/apache/tools/ant/util/DateUtils.java
+++ src/main/org/apache/tools/ant/util/DateUtils.java
@@ -22,12 +22,21 @@ import java.text.DateFormat;
 import java.text.MessageFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.AbstractMap;
 import java.util.Calendar;
+import java.time.Instant;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Map;
+import java.util.Optional;
 import java.util.TimeZone;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import org.apache.tools.ant.MagicNames;
+import org.apache.tools.ant.Project;
 
 /**
  * Helper methods to deal with date/time formatting with a specific
@@ -370,5 +379,74 @@ public final class DateUtils {
             + (m.group(5) == null ? "+00" : m.group(5))
             + (m.group(6) == null ? "00" : m.group(6));
         return iso8601WithTimeZone.get().parse(normISO);
+    }
+
+    /**
+     * Name od the environment variable used to set timestamps for reproducible builds.
+     *
+     * @see "https://reproducible-builds.org/docs/source-date-epoch/"
+     * @since Ant 1.10.18
+     */
+    public static final String ENV_SOURCE_DATE_EPOCH = "SOURCE_DATE_EPOCH";
+
+    /**
+     * Consults the magic properties {@link MagicNames.TSTAMP_NOW_ISO} and {@link MagicNames.TSTAMP_NOW} as well as the
+     * environment variable {@code SOURCE_DATE_EPOCH} for predefined values of "now" and falls back to {@code new
+     * Date()} if neiter is set.
+     *
+     * <p>{@code SOURCE_DATE_EPOCH} takes precendence over {@link MagicNames.TSTAMP_NOW_ISO} which in turn takes precendence over {@link MagicNames.TSTAMP_NOW}.</p>
+     *
+     * @param project Project instance to use when looking up the magic properties.
+     * @return a tuple of "now" and a boolean flag that indicates whether {@code SOURCE_DATE_EPOCH} has been set.
+     * @since Ant 1.10.18
+     */
+    public static Map.Entry<Date, Boolean> getNow(Project project) {
+        final String epoch = System.getenv(ENV_SOURCE_DATE_EPOCH);
+        if (epoch != null) {
+            // Value of SOURCE_DATE_EPOCH will be an integer, representing seconds.
+            try {
+                Date d = new Date(Long.parseLong(epoch) * 1000L);
+                project.log("Honouring environment variable " + ENV_SOURCE_DATE_EPOCH + " which has been set to " + epoch);
+                return new AbstractMap.SimpleImmutableEntry(d, true);
+            } catch(NumberFormatException e) {
+                // ignore
+                project.log("Ignoring invalid value '" + epoch + "' for " + ENV_SOURCE_DATE_EPOCH
+                            + " environment variable", Project.MSG_DEBUG);
+            }
+        }
+        return new AbstractMap.SimpleImmutableEntry(getNowAsDate(project), false);
+    }
+
+    private static Date getNowAsDate(Project p) {
+        Optional<Date> now = getNowAsDate(
+            p,
+            MagicNames.TSTAMP_NOW_ISO,
+            s -> Date.from(Instant.parse(s)),
+            (k, v) -> "magic property " + k + " ignored as '" + v + "' is not in valid ISO pattern"
+        );
+        if (now.isPresent()) {
+            return now.get();
+        }
+
+        now = getNowAsDate(
+            p,
+            MagicNames.TSTAMP_NOW,
+            s -> new Date(1000 * Long.parseLong(s)),
+            (k, v) -> "magic property " + k + " ignored as " + v + " is not a valid number"
+        );
+        return now.orElseGet(Date::new);
+    }
+
+    private static Optional<Date> getNowAsDate(Project p, String propertyName, Function<String, Date> map,
+                                               BiFunction<String, String, String> log) {
+        String property = p.getProperty(propertyName);
+        if (property != null && !property.isEmpty()) {
+            try {
+                return Optional.ofNullable(map.apply(property));
+            } catch (Exception e) {
+                p.log(log.apply(propertyName, property));
+            }
+        }
+        return Optional.empty();
     }
 }
